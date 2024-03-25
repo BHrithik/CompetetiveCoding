9e6cdb65eb78cb994f99e20364149061b5a95749
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-03-25 01:02:32 -0700

    Time: 71 ms (5.51%) | Memory: 16.6 MB (66.83%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,33 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        def AllRotten(grid):
+            for i in range(0,len(grid)):
+                for j in range(0,len(grid[0])):
+                    if grid[i][j] == 1:
+                        return False
+            return True
+        def rot(grid):
+            newGrid = deepcopy(grid)
+            for i in range(0,len(grid)):
+                for j in range(0,len(grid[0])):
+                    if grid[i][j] == 2:
+                        # print(f"found rotten at {i} {j}")
+                        if i-1 >=0 and grid[i-1][j] == 1:
+                            newGrid[i-1][j] = 2
+                        if j-1 >=0 and grid[i][j-1] == 1:
+                            newGrid[i][j-1] = 2
+                        if j+1 <len(grid[0]) and grid[i][j+1] == 1:
+                            newGrid[i][j+1] = 2
+                        if i+1 <len(grid) and grid[i+1][j] == 1:
+                            newGrid[i+1][j] = 2
+            return newGrid
+        prevGrid = []
+        time = 0
+        while grid != prevGrid and not AllRotten(grid):
+            prevGrid = deepcopy(grid)
+            grid = rot(grid)
+            time = time +1
+        if AllRotten(grid):
+            return time
+        else:
+            return -1
