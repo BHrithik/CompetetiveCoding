8e1dd03a0d69ef539fd21839ee6a60e7ae0a2b7d
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-04-05 20:38:08 -0700

    Time: 37 ms (44.74%) | Memory: 16.4 MB (92.99%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -4,21 +4,4 @@ class Solution:
             return '0'
         if num<0:
             num += 2 ** 32
-        hexStr = ""
-        hexDict = {
-            10: 'a',
-            11: 'b',
-            12: 'c',
-            13: 'd',
-            14: 'e',
-            15: 'f',
-        }
-        while num > 0:
-            rem = num%16
-            if rem < 10:
-                hexStr = str(rem) + hexStr
-            else:
-                print(rem)
-                hexStr = hexDict[rem] + hexStr 
-            num = num//16
-        return hexStr
+        return hex(num)[2:]
