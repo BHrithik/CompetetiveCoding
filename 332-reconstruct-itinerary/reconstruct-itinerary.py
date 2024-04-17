4286c55e0231d0518e31d2a674f6881390c2b3ae
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-04-17 01:37:23 -0700

    Time: 82 ms (22.54%) | Memory: 16.9 MB (34.16%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,33 +1,15 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # Create a dictionary to hold all tickets with the source as the key and destinations as the values
-        ticketDict = defaultdict(list)
-        for src, dst in tickets:
-            ticketDict[src].append(dst)
-        
-        # Sort each destination list in reverse order (for efficient pop from the end)
+        ticketDict = defaultdict(lambda:[])
+        for i in tickets:
+            ticketDict[i[0]].append(i[1])
+        ticketDict['JFK']
+        print(ticketDict)
         for src in ticketDict:
             ticketDict[src].sort(reverse=True)
-        
-        # The itinerary list
-        itinerary = []
-
-        def dfs(airport):
-            # Visit all the destinations from the current airport
-            while ticketDict[airport]:
-                # Get the next destination with the smallest lexical order
-                next_airport = ticketDict[airport].pop()
-                dfs(next_airport)
-            # Append the airport to itinerary after visiting all possible routes from it
-            itinerary.append(airport)
-        
-        # Start the DFS from 'JFK'
-        dfs('JFK')
-
-        # Since we add airports in reverse order (post-visiting), we need to reverse the result
-        return itinerary[::-1]
-
-# Usage
-# sol = Solution()
-# result = sol.findItinerary([["JFK","SFO"], ["JFK","ATL"], ["SFO","ATL"], ["ATL","JFK"], ["ATL","SFO"]])
-# print(result)  # Example output: ["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
+        path, stack = [], ['JFK']
+        while stack:
+            while ticketDict[stack[-1]]:
+                stack.append(ticketDict[stack[-1]].pop())
+            path.append(stack.pop())
+        return path[::-1]
