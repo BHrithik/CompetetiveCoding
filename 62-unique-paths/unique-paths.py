2c12ca8b655e7d7aaf7e0986ff6d6e1e682ca796
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-04-19 12:21:14 -0700

    Time: 34 ms (69.66%) | Memory: 16.5 MB (86.52%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,12 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        A = defaultdict(int)
-        for i in range(m):
-            A[(i, n-1)] = 1
-        for j in range(n):
-            A[(m-1, j)] = 1
-        for i in range(m-2, -1, -1):
-            for j in range(n-2, -1, -1):
-                A[(i, j)] = A[(i+1, j)] + A[(i, j+1)]        
-        print(A)
-        return A[(0, 0)]
+        grid = [[1] * n for _ in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                grid[i][j]= grid[i-1][j]+grid[i][j-1]
+        return grid[-1][-1]
+        # A = defaultdict(int)
+        # for i in range(m):
+        #     A[(i, n-1)] = 1
+        # for j in range(n):
+        #     A[(m-1, j)] = 1
+        # for i in range(m-2, -1, -1):
+        #     for j in range(n-2, -1, -1):
+        #         A[(i, j)] = A[(i+1, j)] + A[(i, j+1)]        
+        # print(A)
+        # return A[(0, 0)]
