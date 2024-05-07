586e4c0738e40b034965044f0b0efea23a0e9f52
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-05-06 22:45:32 -0700

    Time: 612 ms (55.04%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,15 +1,7 @@
-# Write your MySQL query statement below
-WITH Accepters AS
-    (SELECT accepter_id AS id, COUNT(accepter_id) AS num
-    FROM RequestAccepted
-    GROUP BY accepter_id),
-Requesters AS
-    (SELECT requester_id AS id, COUNT(requester_id) AS num
-    FROM RequestAccepted
-    GROUP BY requester_id),
-Final AS
-    (SELECT id, sum(num) as num FROM
-        (SELECT * FROM Accepters UNION ALL SELECT * FROM Requesters) AS a
-    GROUP BY id)
-SELECT * FROM Final
+WITH CTE AS (
+    SELECT REQUESTER_ID as id FROM REQUESTACCEPTED
+    UNION ALL
+    SELECT ACCEPTER_ID as id FROM REQUESTACCEPTED
+)
+SELECT id, count(id) as num FROM CTE group by id
 ORDER BY num DESC LIMIT 1;
