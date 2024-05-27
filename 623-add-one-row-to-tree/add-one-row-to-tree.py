adf47b041895e6ba7815201f8b79636e83f0b298
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-05-27 13:34:37 -0700

    Time: 49 ms (41.25%) | Memory: 17.8 MB (76.89%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        def add(root, val, depth, cur_depth):
+            if depth == 1:
+                new_root = TreeNode(val)
+                new_root.left = root
+                return new_root
+            if not root:
+                return None
+            if cur_depth == depth-1:
+                leftman = root.left
+                rightman = root.right
+                root.left = TreeNode(val)
+                root.right = TreeNode(val)
+                root.left.left = leftman
+                root.right.right = rightman
+                return root
+            root.left = add(root.left,val,depth,cur_depth+1)
+            root.right = add(root.right,val,depth,cur_depth+1)
+            return root
+        
+        return add(root,val,depth,1)
+
+                
+
