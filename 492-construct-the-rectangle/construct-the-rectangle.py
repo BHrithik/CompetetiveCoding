b119602d0413a1e1ab6cab07dcae085643d7b963
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-05-27 21:59:05 -0700

    Time: 33 ms (81.55%) | Memory: 16.5 MB (71.09%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.py
+++ 492-construct-the-rectangle/construct-the-rectangle.py
@@ -1,7 +1,7 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         l,w = 0, 0
-        for i in range(1,int(area/2+2)):
+        for i in range(1,int(area**0.5)+1):
             if area%i == 0:
                 if i <= area/i:
                     l = int(area/i)
