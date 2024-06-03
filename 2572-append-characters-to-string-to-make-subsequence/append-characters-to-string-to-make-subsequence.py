6c8e49e8f0a8719d8f77ca95b6fcb87cb79e8c63
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-06-02 18:52:37 -0700

    Time: 681 ms (5.59%) | Memory: 21.3 MB (5.43%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -4,27 +4,24 @@ class Solution:
             return 0
         if t[0] not in s:
             return len(t)
-        
         sDict = {}
-        for i in range(len(s)):
+        for i in range(0,len(s)):
             if s[i] not in sDict:
                 sDict[s[i]] = [i]
             else:
                 sDict[s[i]].append(i)
-        
         currentIndex = -1
         lastIndex = 0
-        
-        for i in range(len(t)):
+        for i in range(0,len(t)):
             if t[i] in sDict and sDict[t[i]]:
-                possible_positions = [pos for pos in sDict[t[i]] if pos > currentIndex]
-                if possible_positions:
-                    currentIndex = possible_positions[0]
+                possibleIndexes = [element for element in sDict[t[i]] if element > currentIndex]
+                if possibleIndexes:
+                    currentIndex = possibleIndexes[0]
                     sDict[t[i]].remove(currentIndex)
                     lastIndex = i
                 else:
                     break
             else:
                 break
-        
-        return len(t) - lastIndex - 1
+        return len(t)-lastIndex-1
+
