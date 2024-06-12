dabe70dbb66e0e9e3c1f7e6603a8a9324c957096
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-06-12 09:44:34 -0700

    Time: 31 ms (90.73%) | Memory: 16.3 MB (97.30%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        counts = [0,0,0]
+        counts = defaultdict(int)
         for n in nums:
             counts[n] += 1
         n = 0
-        for i in range(len(counts)):
+        for i in sorted(counts.keys()):
             for j in range(counts[i]):
                 nums[n] = i
                 n = n+1
