0f09a60c5c7725fe8b98e98ff20cb352f1a063c7
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-04 21:20:42 -0500

    Time: 371 ms (32.89%) | Memory: 30.1 MB (74.85%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -2,14 +2,19 @@ class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
         if len(nums)==1:
             return 0
+        ans = 0
         minNum = min(nums)
         maxNum = max(nums)
-        nums2 = nums[::-1]
         posMin = nums.index(minNum)
-        posMax = len(nums)-nums2.index(maxNum)-1
+        nums = nums[::-1]
+        posMax = len(nums)-nums.index(maxNum)-1
         if posMin > posMax:
-            return posMin-1+len(nums)-posMax-1
+            #we can use a common move to use minimum moves
+            movesForMin = posMin-1
+            movesForMax = len(nums)-posMax-1
         if posMin < posMax:
-            return posMin+len(nums)-posMax-1
+            movesForMin = posMin
+            movesForMax = len(nums)-posMax-1
         if posMin==posMax:
             return 0
+        return movesForMin+movesForMax
