9c0ac07d6399bdb6933c20be4cabbe8ba186b3bf
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-04 21:18:07 -0500

    Time: 357 ms (78.68%) | Memory: 30.1 MB (74.85%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -7,17 +7,9 @@ class Solution:
         nums2 = nums[::-1]
         posMin = nums.index(minNum)
         posMax = len(nums)-nums2.index(maxNum)-1
-        print("min num",minNum)
-        print("Position of min",posMin)
-        print("max num",maxNum)
-        print("Position of max",posMax)
         if posMin > posMax:
-            #we can use a common move to use minimum moves
-            movesForMin = posMin-1
-            movesForMax = len(nums)-posMax-1
+            return posMin-1+len(nums)-posMax-1
         if posMin < posMax:
-            movesForMin = posMin
-            movesForMax = len(nums)-posMax-1
+            return posMin+len(nums)-posMax-1
         if posMin==posMax:
             return 0
-        return movesForMin+movesForMax
