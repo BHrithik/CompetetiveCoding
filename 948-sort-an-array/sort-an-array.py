4bbd420b42dc7fab8a6dffc8479aa512aae89d4c
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-24 19:40:13 -0500

    Time: 556 ms (89.54%) | Memory: 27.4 MB (20.37%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,24 +1,20 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums) == 1:
-            return nums
-        mid = len(nums)//2
-        left = nums[:mid]
-        right = nums[mid:]
-        left_sorted_array = self.sortArray(left)
-        right_sorted_array = self.sortArray(right)
-        return self.MergeArray(left_sorted_array,right_sorted_array)
-    
-    def MergeArray(self, arr1:List[int], arr2: List[int]) -> List[int]:
-        i,j = 0, 0
-        res = []
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] < arr2[j]:
-                res.append(arr1[i])
-                i = i+1
-            else:
-                res.append(arr2[j])
-                j = j+1
-        res.extend(arr1[i:])
-        res.extend(arr2[j:])
-        return res
+        # Find the minimum and maximum values in the input array
+        min_val, max_val = min(nums), max(nums)
+
+        # Create buckets with a size large enough to accommodate all possible values
+        n = len(nums)
+        buckets = [[] for _ in range(max_val - min_val + 1)]
+
+        # Distribute the elements into the buckets
+        for num in nums:
+            index = num - min_val
+            buckets[index].append(num)
+
+        # Concatenate the sorted buckets into the result
+        result = []
+        for bucket in buckets:
+            result.extend(bucket)
+
+        return result
