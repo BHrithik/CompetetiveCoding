bae92aa984eeb87c5f202afa420690c491e09703
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-26 16:05:25 -0500

    Time: 220 ms (15.62%) | Memory: 22.4 MB (7.13%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        arr =[]
+        def populateArr(root):
+            if not root: return
+            populateArr(root.left)
+            arr.append(root.val)
+            populateArr(root.right)
+        populateArr(root)
+        print(arr)
+        arr.sort()
+        def populateTree(nums):
+            if not nums:
+                return
+            n = len(nums)//2
+            mid = TreeNode(nums[n])
+            mid.left = populateTree(nums[0:n])
+            mid.right = populateTree(nums[n+1:])
+            return mid
+        ans = populateTree(arr)
+        return ans
