00ba200d0c172509523b2ffdde65439267d570ac
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-27 22:22:10 -0500

    Time: 43 ms (80.01%) | Memory: 16.9 MB (24.38%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -6,13 +6,11 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if root.val == 0:
-                return False
-            if root.val == 1:
-                return True
-            if root.val == 2:
-                return dfs(root.left) or dfs(root.right)
-            if root.val == 3:
-                return dfs(root.left) and dfs(root.right)
-        return dfs(root)
+        if root.val == 0:
+            return False
+        if root.val == 1:
+            return True
+        if root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        if root.val == 3:
+            return self.evaluateTree(root.left) and self.evaluateTree(root.right)
