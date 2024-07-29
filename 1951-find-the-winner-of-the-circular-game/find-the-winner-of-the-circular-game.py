970e82c0534da309b9764091796ac0a3e6917e99
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-29 13:58:08 -0500

    Time: 36 ms (70.71%) | Memory: 16.7 MB (7.77%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,11 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        arr = list(range(1, n + 1))  # Initialize the array with numbers from 1 to n
+        arr = []
         start = 0
-        k = k - 1  # We use zero-based indexing
-        while len(arr) > 1:
-            to_be_popped = (start + k) % len(arr)  # Find the index to be removed
+        k = k - 1
+        for i in range(0, n):
+            arr.append(i)
+        for i in range(n - 1):
+            to_be_popped = (start + k) % len(arr)
             print(f"Removed member {arr[to_be_popped]}")
-            arr.pop(to_be_popped)  # Remove the member
-            start = to_be_popped % len(arr)  # Update start index
-        return arr[0]  # Return the last remaining member
+            arr.pop(to_be_popped)
+            start = to_be_popped % len(arr)
+        return arr[0]+1
