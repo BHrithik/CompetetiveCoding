41b7b51539f567b25604c1b8b249c7ea8457a430
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-07-31 01:27:17 -0500

    Time: 37 ms (39.22%) | Memory: 16.4 MB (52.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+        cache = {}
+        def dfs(n):
+            if n == 0:
+                return 1
+            if n in cache:
+                return cache[n]
+            elif n>0:
+                cache[n] = dfs(n-1)+dfs(n-2)
+                return cache[n]
+            else:
+                return 0
+        return dfs(n)
