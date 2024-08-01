cea7875aa09443502ddafe8dfe868c385669c942
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-01 16:53:14 -0500

    Time: 54 ms (5.68%) | Memory: 16.6 MB (44.93%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,12 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        ans = [False]*(len(s)+1)
-        ans[len(s)] = True
-        for i in range(len(s)-1,-1,-1):
-            for word in wordDict:
-                if (i+len(word)) <= len(s) and s[i:i+len(word)] == word:
-                    ans[i] = ans[i+len(word)]
-                if ans[i]:
-                    break
-        return ans[0]
-                
+        cache = {}
+        def dfs(cur_s,rem_s):
+            if rem_s in cache:
+                return cache[rem_s]
+            if len(cur_s) > len(s):
+                return False
+            if cur_s == s:
+                return True
+            res = False
+            for i in wordDict:
+                if rem_s[:len(i)] == i and len(cur_s)+len(i) <= len(s):
+                    res = res or dfs(cur_s+i,rem_s[len(i):])
+            cache[rem_s] = res
+            return cache[rem_s]
+        wordDict.sort(key=lambda x: len(s),reverse = True)
+        return dfs("",s)
