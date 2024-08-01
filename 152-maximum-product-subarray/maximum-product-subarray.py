b2449ac268469c71bd3cac8e1f43944941ba52ef
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-01 14:55:17 -0500

    Time: 263 ms (5.15%) | Memory: 82.5 MB (5.86%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -2,10 +2,9 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         @lru_cache(None)
         def dfs(ii, prod):
-            nonlocal mx
             if ii == len(nums): return
 
-            mx = max(mx,
+            self.mx = max(self.mx,
                 prod * nums[ii], # case for continuing the subarray
                 nums[ii], # case for starting a new subarry from ii
                 prod # case for ending the subarry
@@ -13,6 +12,6 @@ class Solution:
             dfs(ii + 1, prod * nums[ii])
             dfs(ii + 1, nums[ii])
 
-        mx = nums[0]
+        self.mx = nums[0]
         dfs(1, nums[0])
-        return mx
+        return self.mx
