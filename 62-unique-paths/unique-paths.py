86b6a1038b04ff82e60c3b8e1fb47dc4ec086321
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-02 13:54:10 -0500

    Time: 33 ms (74.34%) | Memory: 16.4 MB (64.54%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -5,8 +5,8 @@ class Solution:
             matrix[i][n-1] = 1
         for i in range(n):
             matrix[m-1][i] = 1
-        print(matrix)
         for i in range(m-2,-1,-1):
             for j in range(n-2,-1,-1):
                 matrix[i][j] = matrix[i+1][j]+matrix[i][j+1]
+        print(matrix)
         return matrix[0][0]
