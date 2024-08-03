0c5d945e4d844cbfcda96ea327dd3ae16063e1f2
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-03 02:26:30 -0500

    Time: 226 ms (27.75%) | Memory: 37.9 MB (16.47%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {}
+        self.res = 0
+        def helper(idx,rem_sum):
+            if idx == len(nums) and rem_sum == 0:
+                return 1
+            if idx == len(nums) and rem_sum != 0:
+                return 0
+            if (idx,rem_sum) in cache:
+                return cache[idx,rem_sum]
+            cache[idx,rem_sum] = helper(idx+1,rem_sum+nums[idx])+helper(idx+1,rem_sum-nums[idx])
+            return cache[idx,rem_sum]
+        return helper(0,target)
+
