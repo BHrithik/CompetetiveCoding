4ee2dc51129e6b98a60b6d085ad00c2e220a8930
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-03 18:43:20 -0500

    Time: 41 ms (39.77%) | Memory: 16.6 MB (39.43%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,10 +4,15 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        arr = deque([])
-        for i in range(0,n):            
-            for j in range(i,n):
-                matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(0,n):
-            matrix[i].reverse()
-        
+        l = 0
+        r = n - 1
+        while l < r:
+            for i in range(r-l):
+                top, bottom = l, r
+                topleft = matrix[top][l+i]
+                matrix[top][l+i] = matrix[bottom-i][l]
+                matrix[bottom-i][l] = matrix[bottom][r-i]
+                matrix[bottom][r-i] = matrix[top+i][r]
+                matrix[top+i][r] = topleft
+            l += 1
+            r -= 1
