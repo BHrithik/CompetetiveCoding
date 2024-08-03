b615c362e9d705a69245809d668d6a1fe737b9fd
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-03 18:46:31 -0500

    Time: 85 ms (98.18%) | Memory: 18.3 MB (80.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,38 +2,59 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
         l = 0
-        r = n-1
+        r = n - 1
+        maxLeft = height[l]
+        maxRight = height[r]
         water = 0
-        leftmax,rightmax = height[l], height[r]
         while l < r:
-            if leftmax < rightmax:
+            if maxLeft < maxRight:
                 l += 1
-                leftmax = max(leftmax,height[l])
-                water += max(0,leftmax-height[l])
+                maxLeft = max(maxLeft,height[l])
+                water += max(0,maxLeft-height[l])
             else:
                 r -= 1
-                rightmax = max(rightmax,height[r])
-                water += max(0,rightmax-height[r])
+                maxRight = max(maxRight,height[r])
+                water += max(0,maxRight-height[r])
         return water
+                
+        n = len(height)
+        # l = 0
+        # r = n-1
+        # water = 0
+        # leftmax,rightmax = height[l], height[r]
+        # while l < r:
+        #     if leftmax < rightmax:
+        #         l += 1
+        #         leftmax = max(leftmax,height[l])
+        #         water += max(0,leftmax-height[l])
+        #     else:
+        #         r -= 1
+        #         rightmax = max(rightmax,height[r])
+        #         water += max(0,rightmax-height[r])
+        # return water
 
                 
-#         matrix = [[0]*n for _ in range(max(height))]
-#         # print(matrix)      
-#         for i in range(0,len(height)):
-#             for j in range(0,height[i]):
-#                 matrix[j][i] = 1
-#         matrix = matrix
-#         water = 0
-#         for i in range(max(height)):
-#             leftFound = False
-#             for j in range(n):
-#                 if leftFound and matrix[i][j] == 0 and sum(matrix[i][j:]) > 0:
-#                     water += 1
-#                     matrix[i][j] = 2
-#                 if not leftFound and matrix[i][j] == 1:
-#                     leftFound = True
-#         print(matrix[::-1])
-#         return water
+        # matrix = [[0]*n for _ in range(max(height))]
+        # print(matrix)      
+        # for i in range(0,len(height)):
+        #     for j in range(0,height[i]):
+        #         matrix[j][i] = 1
+        # matrix = matrix
+        # water = 0
+        # for i in range(max(height)):
+        #     matrix = [1 if wall > i else 0 for wall in height]
+        #     if sum(matrix) == 1:
+        #         continue
+        #     leftFound = False
+        #     for j in range(n):
+        #         if leftFound and matrix[j] == 0 and sum(matrix[j:]) > 0:
+        #             water += 1
+        #             # matrix[j] = "W"
+        #         if not leftFound and matrix[j] == 1:
+        #             leftFound = True
+        #     # print(matrix) 
+        # # print(matrix[::-1])
+        # return water
 
 # # [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
 # # [0, 0, 0, 1, 2, 2, 2, 1, 1, 2, 1, 0]
