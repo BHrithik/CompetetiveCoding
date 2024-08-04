3a9ab2116625af4b997bc49fb18a2e3f23036a21
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-04 18:30:47 -0500

    Time: 294 ms (12.90%) | Memory: 26.3 MB (9.39%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,29 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left2right = []
+        cur_pro = 1
+        for i in nums:
+            cur_pro *= i
+            left2right.append(cur_pro)
+        right2left = []
+        cur_pro = 1
+        for i in range(len(nums)-1,-1,-1):
+            cur_pro *= nums[i]
+            right2left.append(cur_pro)
+        right2left = right2left[::-1]
         res = []
-        n = len(nums)
-        prefix_arr = [0]*n
-        postfix_arr = [0]*n
-        prefix = 1
-        for i in range(0,n):
-            prefix *= nums[i]
-            prefix_arr[i] = prefix
-        print(prefix_arr)
-        postfix = 1
-        for i in reversed(range(0,n)):
-            postfix *= nums[i]
-            postfix_arr[i] = postfix
-        print(postfix_arr)
-        res = [0]*n
-        for i in range(0,n):
-            if i-1 < 0:
-                prod1 = 1
-            else:
-                prod1 = prefix_arr[i-1]
-            if i+1 >= n:
-                prod2 = 1
-            else:
-                prod2 = postfix_arr[i+1]
-            res[i] = prod1*prod2
-        print(res)
+        print(left2right)
+        print(right2left)
+        for i in range(len(nums)):
+            p1 = left2right[i-1] if i > 0 else 1
+            p2 = right2left[i+1] if i < len(right2left)-1 else 1
+            res.append(p1*p2)
         return res
