2509bab3ba6ccb1df601ab32389fe38b11beb1c1
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-04 18:14:28 -0500

    Time: 459 ms (94.84%) | Memory: 23.6 MB (57.53%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,7 +2,7 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         min_p = prices[0]+fee
         res= 0
-        for p in prices:
+        for p in prices[1:]:
             if min_p < p:
                 res += p-min_p
                 min_p = p
