49e488c42494e58fb969e80ba1e5d3acd6cb84b6
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-04 18:32:17 -0500

    Time: 272 ms (47.56%) | Memory: 26.2 MB (19.11%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -12,8 +12,6 @@ class Solution:
             right2left.append(cur_pro)
         right2left = right2left[::-1]
         res = []
-        print(left2right)
-        print(right2left)
         for i in range(len(nums)):
             p1 = left2right[i-1] if i > 0 else 1
             p2 = right2left[i+1] if i < len(right2left)-1 else 1
