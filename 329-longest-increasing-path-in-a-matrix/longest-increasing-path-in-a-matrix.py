9a0676d347e0d899e36a14a5f587b0fa48bec964
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-05 17:16:12 -0500

    Time: 340 ms (38.94%) | Memory: 21.1 MB (42.07%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        cache = {}
+        self.maxLen = 0
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            length = 1  # At least the cell itself
+            for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                if 0 <= x < len(matrix) and 0 <= y < len(matrix[0]) and matrix[x][y] > matrix[i][j]:
+                    length = max(length, 1 + dfs(x, y))
+            
+            cache[(i, j)] = length
+            return length
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                self.maxLen = max(self.maxLen,dfs(i,j))
+        return self.maxLen
