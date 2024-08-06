401aa5844bbf1f6ed10e944194decbce46921d64
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-06 11:28:42 -0500

    Time: 468 ms (6.32%) | Memory: 28.2 MB (95.81%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,17 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pos_seen = 0
-        neg_seen = 0
-        for i in nums:
-            cur_mask = 1 << abs(i)
-            if i >= 0:
-                if pos_seen & cur_mask:
-                    return True
-                else:
-                    pos_seen ^= cur_mask
-            else:
-                if neg_seen & cur_mask:
-                    return True
-                else:
-                    neg_seen ^= cur_mask
+        nums.sort()
+        for i in range(0,len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
         return False
