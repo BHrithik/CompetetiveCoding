a338584581f7f8a978ff35558543dfe5586ff4d1
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-06 15:07:29 -0500

    Time: 234 ms (19.43%) | Memory: 17.8 MB (8.15%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        wordDict = Counter(word)
+        keys = sorted(wordDict.keys(),key=lambda x: wordDict[x],reverse = True)
+        keyPad = {}
+        count = 0
+        cur_count = 1
+        for i in keys:
+            count += 1
+            if count <= 8:
+                keyPad[i] = 1
+            elif count <= 16:
+                keyPad[i] = 2
+            elif count <= 24:
+                keyPad[i] = 3
+            else:
+                keyPad[i] = 4
+        print(keyPad)
+        res = 0
+        for i in word:
+            res += keyPad[i]
+        return res
+            
+
+
