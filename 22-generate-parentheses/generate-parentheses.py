21bb7133acfe43189d92564a898c878c2bbc7a65
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-07 01:57:18 -0500

    Time: 67 ms (5.04%) | Memory: 17.8 MB (12.16%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,7 +1,8 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         length = n*2
-        res = []
+        res = set()
+        cache = defaultdict(list)
         def valid(s):
             stack = []
             for char in s:
@@ -15,12 +16,16 @@ class Solution:
             return len(stack) == 0
         def helper(s):
             if len(s) == length:
-                if valid(s):
-                    res.append(s)
+                if s not in res and valid(s):
+                    res.add(s)
                 return
+            cache[len(s)].append(s)
+            if cache[length-len(s)]:
+                for each in cache[length-len(s)]:
+                    helper(s+each)
             if s.count('(') < n:
                 helper(s + '(')
             if s.count(')') < s.count('('):
                 helper(s + ')')
         helper("")
-        return res
+        return list(res)
