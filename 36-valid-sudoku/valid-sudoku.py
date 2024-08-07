958716dcf10e9e93ee3248064570f178438ae271
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-06 20:53:59 -0500

    Time: 93 ms (51.87%) | Memory: 16.5 MB (43.23%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,21 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def validate(i,j):
-            for x in range(n):
-                if x!= i and board[i][j] == board[x][j]: #All in the same column
-                    return False
-                if x!= j and board[i][j] == board[i][x]: #All in the same row
-                    return False
-            p1 = (i//3*3,j//3*3) # Caclulate start point of box
-            for x in range(p1[0],p1[0]+3):
-                for y in range(p1[1],p1[1]+3):
-                    if (x,y) != (i,j) and board[i][j] == board[x][y]:
-                        return False
-            return True
+        boxes = defaultdict(set)
+        cols = defaultdict(set)
+        rows = defaultdict(set)
         n = len(board)
         for i in range(n):
             for j in range(n):
-                if board[i][j] != "." and not validate(i,j):
+                if board[i][j] == ".":
+                    continue
+                box_n = (i//3*3,j//3*3)
+                if board[i][j] in cols[j] or board[i][j] in rows[i] or board[i][j] in boxes[box_n]:
                     return False
+                cols[j].add(board[i][j])
+                rows[i].add(board[i][j])
+                boxes[box_n].add(board[i][j])
         return True
     
