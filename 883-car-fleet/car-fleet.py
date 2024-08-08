305b5563c104201af8659f758b6d37bbc1c311dc
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-08 14:01:59 -0500

    Time: 768 ms (20.44%) | Memory: 36.3 MB (83.94%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,20 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # def count_fleets(target, position, speed):
-    # Combine position and speed into a list of tuples and sort by position in descending order
-        cars = sorted(zip(position, speed), reverse=True)
-        
-        fleets = 0
-        current_time = 0
-        
-        # Iterate over the sorted list
-        for pos, spd in cars:
-            time_to_target = (target - pos) / spd
-            
-            # If the current car's time to target is greater than the current fleet's time,
-            # it means this car forms a new fleet.
-            if time_to_target > current_time:
-                fleets += 1
-                current_time = time_to_target
-        
-        return fleets
+        car_pos = [[pos,speed] for pos,speed in zip(position,speed)]
+
+        stack = []
+        for pos,speed in sorted(car_pos,reverse=True):
+            if stack and (target-stack[-1][0])/stack[-1][1] >= (target-pos)/speed:
+                continue
+            else:
+                stack.append([pos,speed])
+        return len(stack)
