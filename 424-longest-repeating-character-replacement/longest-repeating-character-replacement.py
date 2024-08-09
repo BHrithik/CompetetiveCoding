68313ba60836bc09c2883ec9d714b98a294c0893
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-09 00:11:23 -0500

    Time: 75 ms (87.06%) | Memory: 16.6 MB (32.23%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,9 +3,11 @@ class Solution:
         l = 0
         cur_s = defaultdict(int)
         res = 0
+        maxF = 0
         for r in range(0,len(s)):
             cur_s[s[r]] += 1
-            while r-l+1 - max(cur_s.values()) > k:
+            maxF = max(cur_s[s[r]],maxF)
+            while r-l+1 - maxF > k:
                 cur_s[s[l]] -= 1
                 l += 1
             res = max(res,r-l+1)
