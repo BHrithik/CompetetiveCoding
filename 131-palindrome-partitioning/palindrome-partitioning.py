ac9a10caa885c02d2642b96e149a9bc925e5f5b0
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-13 18:34:56 -0500

    Time: 434 ms (98.81%) | Memory: 35.2 MB (63.80%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -6,10 +6,10 @@ class Solution:
             if i>= len(s):
                 res.append(part.copy())
                 return
-            for j in range(i,len(s)):
-                if s[i:j+1] == s[i:j+1][::-1]:
-                    part.append(s[i:j+1])
-                    dfs(j+1)
+            for j in range(i+1,len(s)+1):
+                if s[i:j] == s[i:j][::-1]:
+                    part.append(s[i:j])
+                    dfs(j)
                     part.pop()
         dfs(0)
         return res
