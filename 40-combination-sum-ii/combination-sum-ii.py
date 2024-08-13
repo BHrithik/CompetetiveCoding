b0dfa00d1e80c1f4aa770fde351ca740d90c8fb5
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-13 12:22:55 -0500

    Time: 71 ms (29.56%) | Memory: 16.8 MB (6.94%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = set()
+        subSet = []
+        def bcktrk(i,cur_sum):
+            if cur_sum == target:
+                res.add(tuple(subSet.copy()))
+                return
+            if cur_sum > target or i>= len(candidates):
+                return
+            subSet.append(candidates[i])
+            bcktrk(i+1,cur_sum+candidates[i])
+            subSet.pop()
+            while i+1 < len(candidates) and candidates[i+1] == candidates[i]:
+                i = i+1
+            bcktrk(i+1, cur_sum)
+        bcktrk(0,0)
+        return res
