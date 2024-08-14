29ab414705be0fc135ab48c7f37ac2bebd00eca3
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-14 17:58:53 -0500

    Time: 34 ms (67.58%) | Memory: 16.7 MB (72.98%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,29 +1,25 @@
-from collections import defaultdict, deque
-from typing import List
-
 class Twitter:
 
     def __init__(self):
-        self.feed = defaultdict(deque)  # Use a defaultdict to maintain feed per user
-        self.followers = defaultdict(set)
-        self.tweets = deque()  # Store all tweets
-    
+        self.feed = deque() 
+        self.followers = defaultdict(lambda: set())
+        self.tweets = deque()
+
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweets.appendleft((userId, tweetId))  # Add new tweets to the front of the deque
-    
+        self.tweets.appendleft((userId,tweetId))
+
     def getNewsFeed(self, userId: int) -> List[int]:
-        # Collect tweets from the user's feed and their followees
+        count = 0
         feed = []
-        following = self.followers[userId]  # Get users whom the user follows
-        following.add(userId)  # Add the user itself to the following set
-        
-        # Iterate through the tweets in reverse order (most recent first)
-        for tweet_user, tweet_id in self.tweets:
-            if tweet_user in following:
-                feed.append(tweet_id)
-                if len(feed) == 10:  # Return only the most recent 10 tweets
-                    break
+        following = self.followers[userId]
+        following.add(userId)
+        for id,tweet in self.tweets:
+            if id in following:
+                feed.append(tweet)
+            if len(feed)==10:
+                break
         return feed
+
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followers[followerId].add(followeeId)
@@ -31,3 +27,11 @@ class Twitter:
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.followers[followerId]:
             self.followers[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
