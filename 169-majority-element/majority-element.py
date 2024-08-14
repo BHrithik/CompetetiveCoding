d6c115ddf5eaac6e59b0e7957e255cc949925062
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-13 21:59:12 -0500

    Time: 170 ms (58.96%) | Memory: 17.9 MB (93.44%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,3 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(Counter(nums).keys(),key=lambda x: Counter(nums)[x],reverse=True)[0]
+        nums.sort()
+        return nums[len(nums)//2]
