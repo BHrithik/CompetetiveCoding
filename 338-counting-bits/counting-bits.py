ea05de3effa5845aed3f32acb3c62cfa48042809
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-14 00:13:14 -0500

    Time: 41 ms (99.80%) | Memory: 23.2 MB (78.32%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,15 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def binary(a):
-            s = ''
-            while a:
-                s = str(a%2) + s
-                a = a//2
-            return s
-        ans = [0]
-        for i in range(1,n+1):
-            bitDict = Counter(binary(i))
-            print(bitDict)
-            print(bitDict['1'])
-            ans.append(bitDict['1'])
-        return ans
+        arr = []
+        for i in range(n+1):
+            arr.append(i.bit_count())
+        return arr
