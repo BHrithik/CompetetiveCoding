ea1723779130a536f7d7340489162141e54173bd
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-15 00:07:52 -0500

    Time: 79 ms (32.63%) | Memory: 20.1 MB (14.80%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,11 +1,31 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals.sort()
-        mergedIntervals = []
-        for interval in intervals:
-            if not mergedIntervals or mergedIntervals[-1][1] < interval[0]:
-                mergedIntervals.append(interval)
+        if not intervals:
+            return [newInterval]
+        if not newInterval:
+            return intervals
+        inserted = False
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            if start >= newInterval[0]:
+                intervals.insert(i, newInterval)
+                inserted = True
+                break
+            elif end >= newInterval[0]:
+                intervals.insert(i + 1, newInterval)
+                inserted = True
+                break
+        if not inserted:
+            intervals.append(newInterval)
+        print(intervals)
+        l = 1
+        res = [intervals[0]]
+        while l < len(intervals):
+            if intervals[l][0] <= res[-1][1]: # We need to meerge here
+                # res[-1][0] = min(res[-1][0],intervals[l][0])
+                res[-1][1] = max(res[-1][1],intervals[l][1])
             else:
-                mergedIntervals[-1][1] = max(mergedIntervals[-1][1],interval[1])
-        return mergedIntervals
+                res.append(intervals[l])
+            l+=1
+        return res
+            
