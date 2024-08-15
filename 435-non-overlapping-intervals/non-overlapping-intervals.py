6f1a7a768c1684480fcc4a592848d1c74c91f0c6
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-14 22:58:37 -0500

    Time: 1246 ms (7.28%) | Memory: 54.7 MB (96.11%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -3,7 +3,6 @@ class Solution:
         intervals.sort()
         count = 0
         for i in range(0,len(intervals)-1):
-            # print(f" {intervals[i+1][0]} > {intervals[i][1]}")
             if intervals[i+1][0] < intervals[i][1]:
                 count+=1
                 intervals[i+1][1] = min(intervals[i][1],intervals[i+1][1])
