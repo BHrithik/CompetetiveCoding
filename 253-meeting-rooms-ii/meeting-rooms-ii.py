f3aab9e34988df3e264963642a998b332cd4f148
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-14 22:08:05 -0500

    Time: 76 ms (44.86%) | Memory: 20.1 MB (28.44%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,14 +1,11 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        def getTime(intervals):
-            if not intervals: return 0
-            conflicts = []
-            for i in range(len(intervals)-1):
-                if intervals[i][1] > intervals[i+1][0]:
-                    conflicts.append(intervals[i+1])
-                    intervals[i+1]=intervals[i]
-            return 1 + getTime(conflicts)
-
+        if not intervals: return 0
         intervals.sort()
-        return getTime(intervals)
+        conflicts = []
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
+                conflicts.append(intervals[i+1])
+                intervals[i+1]=intervals[i]
+        return 1 + self.minMeetingRooms(conflicts)
         
