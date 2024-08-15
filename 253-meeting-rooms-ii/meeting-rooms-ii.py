f8f3c21c2445f10d23b3d02902370aff575d2886
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-14 22:07:20 -0500

    Time: 84 ms (12.13%) | Memory: 20.1 MB (28.44%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -3,11 +3,8 @@ class Solution:
         def getTime(intervals):
             if not intervals: return 0
             conflicts = []
-            intervals.sort()
             for i in range(len(intervals)-1):
-                end = intervals[i][1]
-                start = intervals[i+1][0]
-                if end > start:
+                if intervals[i][1] > intervals[i+1][0]:
                     conflicts.append(intervals[i+1])
                     intervals[i+1]=intervals[i]
             return 1 + getTime(conflicts)
