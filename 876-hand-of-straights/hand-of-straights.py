21adb996fee45480851926a0ab88a1ea22f6aa8d
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-15 16:51:32 -0500

    Time: 478 ms (13.60%) | Memory: 18.2 MB (83.17%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -3,12 +3,15 @@ class Solution:
         if len(hand)%groupSize != 0:
             return False
         hand.sort()
+        handCounter = Counter(hand)
         n = len(hand)
         for i in range(n//groupSize):
-            cur = hand[0]
+            cur = sorted(handCounter.keys())[0]
             for j in range(0,groupSize):
-                if cur+j in hand:
-                    hand.pop(hand.index(cur+j))
+                if cur+j in handCounter:
+                    handCounter[cur+j] -= 1
+                    if handCounter[cur+j] == 0:
+                        del handCounter[cur+j]
                 else:
                     return False
         return True
