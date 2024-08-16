91c623bb082ccc87f2a2715ffef945f5ba0ea7b0
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-15 19:34:57 -0500

    Time: 1682 ms (12.89%) | Memory: 61.7 MB (61.43%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -2,9 +2,13 @@ class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         good = set()
         for t in triplets:
-            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
-                continue
-            for i in range(3):
-                if t[i] == target[i]:
-                    good.add(i)
+            flag = False
+            for i in range(len(target)):
+                if t[i] > target[i]:
+                    flag = True
+                    break
+            if not flag:
+                for i in range(3):
+                    if t[i] == target[i]:
+                        good.add(i)
         return len(good) == 3
