ab50afb64594d6609ee1acb70e2fc3a9bb273090
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 11:38:20 -0500

    Time: 106 ms (66.92%) | Memory: 17.2 MB (59.16%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,9 +3,7 @@ class Solution:
         res = 0
         directions = [(1,0),(0,1),(-1,0),(0,-1)]
         def dfs(i,j):
-            if not (0<=i<len(grid) and 0<=j<len(grid[0])):
-                return 0
-            if grid[i][j] == 0:
+            if not (0<=i<len(grid) and 0<=j<len(grid[0])) or grid[i][j] == 0:
                 return 0
             grid[i][j] = 0
             return 1+dfs(i+1,j)+dfs(i,j+1)+dfs(i-1,j)+dfs(i,j-1)
