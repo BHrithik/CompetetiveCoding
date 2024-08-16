6155a0ea7df375144ab9dc37e945c7e330b5697f
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 12:22:36 -0500

    Time: 37 ms (48.13%) | Memory: 16.4 MB (70.62%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,24 +1,42 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftstack = []
-        starstack = []
-        stars = 0
-        for i in range(0,len(s)):
-            if s[i] == "(":
-                leftstack.append(i)
-            elif s[i] == "*":
-                starstack.append(i)
+        leftMin = 0
+        leftMax = 0
+        for i in s:
+            if i == "(":
+                leftMin += 1
+                leftMax += 1
+            elif i == ")":
+                leftMin -= 1
+                leftMax -= 1
             else:
-                if leftstack:
-                    leftstack.pop()
-                elif starstack:
-                    starstack.pop()
-                else:
-                    return False
-        while leftstack and starstack:
-            if leftstack[-1] < starstack[-1]:
-                leftstack.pop()
-                starstack.pop()
-            else:
+                leftMin -= 1
+                leftMax += 1
+            if leftMax <0:
                 return False
-        return False if leftstack else True
+            if leftMin <0:
+                leftMin = 0
+        return leftMin == 0
+
+        # leftstack = []
+        # starstack = []
+        # stars = 0
+        # for i in range(0,len(s)):
+        #     if s[i] == "(":
+        #         leftstack.append(i)
+        #     elif s[i] == "*":
+        #         starstack.append(i)
+        #     else:
+        #         if leftstack:
+        #             leftstack.pop()
+        #         elif starstack:
+        #             starstack.pop()
+        #         else:
+        #             return False
+        # while leftstack and starstack:
+        #     if leftstack[-1] < starstack[-1]:
+        #         leftstack.pop()
+        #         starstack.pop()
+        #     else:
+        #         return False
+        # return False if leftstack else True
