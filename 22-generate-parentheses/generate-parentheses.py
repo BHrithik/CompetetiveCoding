ee1a8bc3bb0c2da9243f48335df89f5a7e49255d
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 11:16:46 -0500

    Time: 35 ms (75.62%) | Memory: 17.1 MB (12.42%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -6,6 +6,10 @@ class Solution:
         def helper(s,left,right):
             if len(s) == length and s not in res:
                 res.add(s)
+                return
+            # if left == right and (n-left,n-right) in cache:
+            #     if s+cache[(n-left,n-right)] not in res:
+            #         res.add(s+cache[(n-left,n-right)])
             if left < n:
                 helper(s+"(",left+1, right)
             if right < left:
