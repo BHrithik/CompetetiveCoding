27971ec28abdbc6d82bdd056de9926a568eec3e9
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-15 20:52:21 -0500

    Time: 44 ms (10.91%) | Memory: 16.6 MB (39.46%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,7 +3,9 @@ class Solution:
         negative = False
         if x < 0:
             negative = True
-        x_str = int(str(abs(x))[::-1])
+        x = abs(x)
+        print(bin(x))
+        x_str = int(str(x)[::-1])
         if x_str > 2**31-1 or x_str < -2**31:
             return 0
         return int(x_str) if not negative else -int(x_str)
