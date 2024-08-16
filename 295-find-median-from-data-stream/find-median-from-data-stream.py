dcd6c786753dbf5d2c7dd9f4e986801b9c6b4fc8
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-15 23:08:55 -0500

    Time: 412 ms (24.73%) | Memory: 38.5 MB (47.79%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,40 +1,18 @@
 class MedianFinder:
+
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.maxheap = []  # Max heap (simulated with negative values)
-        self.minheap = []  # Min heap
+        self.minHeap = []
+        self.maxHeap = []
 
     def addNum(self, num: int) -> None:
-        """
-        Adds a number into the data structure.
-        """
-        heappush(self.maxheap, -num)  # Add to max heap
-        # Move the largest element of max heap to min heap
-        heappush(self.minheap, -heappop(self.maxheap))
-
-        # If maxheap has more elements, move the smallest element of large to small
-        if len(self.minheap) > len(self.maxheap):
-            heappush(self.maxheap, -heappop(self.minheap))
+        heapq.heappush(self.maxHeap,-num)
+        heapq.heappush(self.minHeap,-(heapq.heappop(self.maxHeap)))
+        if len(self.minHeap) > len(self.maxHeap)+1:
+            heapq.heappush(self.maxHeap,-heapq.heappop(self.minHeap))
 
     def findMedian(self) -> float:
-        """
-        Returns the median of current data stream
-        """
-        # If the heaps have equal size, the median is the average of the two heap's top elements
-        if len(self.maxheap) == len(self.minheap):
-            return (-self.maxheap[0] + self.minheap[0]) / 2.0
-        # If the maxheap has more elements, return the top element
-        elif len(self.maxheap) > len(self.minheap):
-            return -self.maxheap[0]
-        # Otherwise, return the top element of the minheap
+        if len(self.minHeap)>len(self.maxHeap):
+            return self.minHeap[0]
         else:
-            return self.minheap[0]
+            return (self.minHeap[0]-self.maxHeap[0])/2
         
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
