871d4a0b01cc49c0fa99a76791adae6d73ad57fc
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 15:49:41 -0500

    Time: 39 ms (97.55%) | Memory: 16.4 MB (98.35%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,106 +1,48 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0
-        q = deque()
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    q.append((i,j))
-                if grid[i][j] == 1:
-                    fresh += 1
-        time = 0
-        while q and fresh > 0:
-            for _ in range(len(q)):
-                r,c = q.popleft()            
-                for di,dj in directions:
-                    nr,nc = r+di,c+dj
-                    if nr < 0 or nr == len(grid) or nc <0 or nc == len(grid[0]) or grid[nr][nc] != 1:
-                        continue
-                    grid[nr][nc] = 2
-                    q.append((nr,nc))
+        # def AllRotten(grid):
+        #     for i in range(0,len(grid)):
+        #         for j in range(0,len(grid[0])):
+        #             if grid[i][j] == 1:
+        #                 return False
+        #     return True
+        def countFreshAndRotten(grid):
+            count =0
+            rotten = []
+            for i in range(0,len(grid)):
+                for j in range(0,len(grid[0])):
+                    if grid[i][j] == 1:
+                        count += 1
+                    if grid[i][j] == 2:
+                        rotten.append((i,j))
+            return count, rotten
+        def rot(rotten,fresh):
+            new_rotten = []
+            for i,j in rotten:
+                if i-1 >=0 and grid[i-1][j] == 1:
+                    grid[i-1][j] = 2
+                    new_rotten.append((i-1,j))
                     fresh -= 1
-            time += 1
-        return -1 if fresh > 0 else time
-
-
-
-
-
-
-
-
-        # oranges = []
-        # time = 0
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[i])):
-        #         if grid[i][j] == 2:
-        #             oranges.append((i,j,time))
-        # r_oranges = deque(oranges)
-        # directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        # max_time = -1
-        # while r_oranges:
-        #     i,j,cur_time = r_oranges.popleft()
-        #     max_time = max(cur_time,max_time)
-        #     for di,dj in directions:
-        #         if 0 <= i+di < len(grid) and 0<= j+dj <len(grid[0]) and grid[i+di][j+dj] == 1:
-        #             grid[i+di][j+dj] = 2
-        #             r_oranges.append((i+di,j+dj,cur_time+1))
-        # flag= True
-        # orange_present = False
-        # for i in grid:
-        #     for j in i:
-        #         if j == 1:
-        #             flag = False
-        #             orange_present = True
-        #         if j == 2:
-        #             orange_present = True
-        # if flag and orange_present:
-        #     return max_time
-        # elif not flag and orange_present:
-        #     return -1
-        # else:
-        #     return 0
-
-
-
-
-
-
-
-
-
-
-        # oranges = []
-        # time = 0
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[i])):
-        #         if grid[i][j] == 2:
-        #             oranges.append((i,j,time))
-        # r_oranges = deque(oranges)
-        # directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        # max_time = -1
-        # while r_oranges:
-        #     i,j,cur_time = r_oranges.popleft()
-        #     max_time = max(cur_time,max_time)
-        #     for di,dj in directions:
-        #         if 0 <= i+di < len(grid) and 0<= j+dj <len(grid[0]) and grid[i+di][j+dj] == 1:
-        #             grid[i+di][j+dj] = 2
-        #             r_oranges.append((i+di,j+dj,cur_time+1))
-        # flag= True
-        # orange_present = False
-        # for i in grid:
-        #     for j in i:
-        #         if j == 1:
-        #             flag = False
-        #             orange_present = True
-        #         if j == 2:
-        #             orange_present = True
-        # if flag and orange_present:
-        #     return max_time
-        # elif not flag and orange_present:
-        #     return -1
-        # else:
-        #     return 0
-
-
+                if j-1 >=0 and grid[i][j-1] == 1:
+                    grid[i][j-1] = 2
+                    new_rotten.append((i,j-1))
+                    fresh -= 1
+                if j+1 <len(grid[0]) and grid[i][j+1] == 1:
+                    grid[i][j+1] = 2
+                    new_rotten.append((i,j+1))
+                    fresh -= 1
+                if i+1 <len(grid) and grid[i+1][j] == 1:
+                    grid[i+1][j] = 2
+                    new_rotten.append((i+1,j))
+                    fresh -= 1
+            return new_rotten, fresh
+        prevGrid = []
+        time = 0
+        fresh,rotten = countFreshAndRotten(grid)
+        while rotten and fresh > 0:
+            rotten,fresh = rot(rotten,fresh)
+            time = time +1
+        if fresh == 0:
+            return time
+        else:
+            return -1
