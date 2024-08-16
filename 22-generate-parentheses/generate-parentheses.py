7521eebc47ac3d8d8b49286be25ebcefba59d3ca
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 11:14:23 -0500

    Time: 44 ms (18.07%) | Memory: 17.1 MB (12.42%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,15 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         length = n*2
-        res = []
+        res = set()
+        cache = defaultdict(list)
         def helper(s,left,right):
-            if len(s) == length:
-                res.append(s)
+            if len(s) == length and s not in res:
+                res.add(s)
             if left < n:
                 helper(s+"(",left+1, right)
             if right < left:
                 helper(s+")",left,right+1)
         helper("",0,0)
-        return res
-
-            
+        return list(res)
