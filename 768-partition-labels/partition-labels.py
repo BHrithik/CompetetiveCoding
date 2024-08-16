a63dc34b032960039871ff29c7a46b73cf5f71b6
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-16 01:30:59 -0500

    Time: 47 ms (22.34%) | Memory: 16.5 MB (28.65%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -2,10 +2,7 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         charDict = {}
         for i in range(0,len(s)):
-            char = s[i]
-            if char not in charDict:
-                charDict[char] = i
-            charDict[char] = i
+            charDict[s[i]] = i
         cur_max = charDict[s[0]]
         res = []
         for r in range(0,len(s)):
@@ -13,16 +10,3 @@ class Solution:
             if r == cur_max:
                 res.append(r+1-sum(res))
         return res
-        # while r < len(s):
-        #     if r == charDict[cur_char]:
-        #         res.append(cur_s)
-        #         cur_s = ""
-        #     else:
-        #         cur_s += s[r]
-        #         if s[r] != cur_char:
-        #             if charDict[s[r]] > charDict[cur_char]:
-        #                 cur_char = s[r]
-        #     r += 1
-        # return res
-
-        
