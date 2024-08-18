cdc5d8167947d938ac871d0bd05cda068b133db7
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-17 20:49:22 -0500

    Time: 16 ms (99.97%) | Memory: 16.7 MB (16.69%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,5 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # #12106
+        # res = 0
+        cache = {}
+        def dfs(i):
+            if i == len(s):
+                return 1
+            if s[i] == "0":
+                return 0
+            if i in cache:
+                return cache[i]
+            cache[i] = dfs(i+1)
+            if i+1 < len(s) and int(s[i] + s[i+1]) <= 26:
+                cache[i] += dfs(i+2)
+            return cache[i]
+        return dfs(0)
+        # val1, val2 = 0, 0
+        # for i in range(len(s)):
+            
         dp = {}
         def dfs(i):
             if i == len(s):
@@ -14,3 +32,16 @@ class Solution:
             dp[i] = res
             return dp[i]
         return dfs(0)
+
+
+            
+
+
+
+
+
+
+
+
+
+
