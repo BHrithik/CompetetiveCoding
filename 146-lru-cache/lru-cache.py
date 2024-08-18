ecd08996e74e28c54d84d50fa0beac1c794d0f42
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-18 13:31:21 -0500

    Time: 592 ms (29.33%) | Memory: 78.4 MB (30.65%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,20 +1,49 @@
-class LRUCache:
+class Node:
+    def __init__(self,key=0,value=0):
+        self.key = key
+        self.value= value
+        self.next,self.prev = None, None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = OrderedDict()
+        self.cap = capacity
+        self.cache = {}
+        self.left, self.right = Node(),Node()
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    # Inserting a node at the most frequent node i.e. the right
+    def insert(self,node):
+        prev_node = self.right.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.right
+        self.right.prev = node
+
+
+    # Removing the left most value in our doubly linked link list
+    def remove(self,node):
+        front = node.next
+        back = node.prev
+        back.next = front
+        front.prev = back
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            value = self.cache.pop(key)
-            self.cache[key] = value
-            return value
+            self.remove(self.cache[key]) # remove it from the current chain
+            self.insert(self.cache[key]) # add it to the left most as it is the most recently used
+            return self.cache[key].value
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.pop(key)
-        elif len(self.cache) == self.capacity:
-            self.cache.popitem(last=False)
-        self.cache[key] = value
+            self.remove(self.cache[key])  # Remove the old node
+        new_node = Node(key, value)
+        self.insert(new_node)
+        self.cache[key] = new_node
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]     
