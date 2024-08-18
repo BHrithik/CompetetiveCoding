31a292898a3818bb4bb521f646a457bc0146a7bd
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-17 19:12:35 -0500

    Time: 34 ms (71.67%) | Memory: 16.5 MB (66.86%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,26 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int: 
-        cache = {}     
-        def dfs(i):
-            if i > len(nums)-1:
-                return 0
-            if i in cache:
-                return cache[i]
-            cache[i]  = nums[i] + max(dfs(i+2),dfs(i+3))
-            return cache[i]
-        return max(dfs(0),dfs(1))
-        
-
-
-
-
-
-
-
-
-        # rob1, rob2 = 0, 0
-        # for i in nums:
-        #     temp = max(rob1+i,rob2)
-        #     rob1 = rob2
-        #     rob2 = temp
-        # return rob2
+        # cache = {}     
+        # def dfs(i):
+        #     if i > len(nums)-1:
+        #         return 0
+        #     if i in cache:
+        #         return cache[i]
+        #     cache[i]  = nums[i] + max(dfs(i+2),dfs(i+3))
+        #     return cache[i]
+        # return max(dfs(0),dfs(1))
+        rob1, rob2 = 0, 0
+        for i in nums:
+            temp = max(rob1+i,rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
