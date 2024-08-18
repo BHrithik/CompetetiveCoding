3b443adcf5308eecc6edd48d311bc59c025b2654
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-17 21:07:22 -0500

    Time: 1181 ms (13.83%) | Memory: 45.3 MB (7.07%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,9 +1,36 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1]*(amount+1)
-        dp[0] = 0
-        for a in range(1,amount+1):
+        res = float('inf')
+        cache = {}
+        def helper(cur_amount):
+            if cur_amount == 0:
+                return 0
+            if cur_amount < 0:
+                return float('inf')
+            if cur_amount in cache:
+                return cache[cur_amount]
+            res = float('inf')
             for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(dp[a],1+dp[a-coin])
-        return dp[amount] if dp[amount] != amount+1 else -1
+                res = min(res,helper(cur_amount-coin)+1)
+            cache[cur_amount] = res
+            return cache[cur_amount]
+        res = helper(amount)
+        return res if res != float('inf') else -1
+
+
+
+
+
+
+
+
+
+
+
+        # dp = [amount+1]*(amount+1)
+        # dp[0] = 0
+        # for a in range(1,amount+1):
+        #     for coin in coins:
+        #         if a - coin >= 0:
+        #             dp[a] = min(dp[a],1+dp[a-coin])
+        # return dp[amount] if dp[amount] != amount+1 else -1
