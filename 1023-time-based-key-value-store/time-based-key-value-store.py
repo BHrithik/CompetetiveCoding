56a24200a7b404581914e362c4222079922023c3
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-18 01:36:52 -0500

    Time: 636 ms (12.98%) | Memory: 74.3 MB (30.44%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -15,9 +15,7 @@ class TimeMap:
         r = len(values)-1
         while l<=r:
             m = (l+r)//2
-            if values[m][1] == timestamp:
-                return values[m][0]
-            elif values[m][1] < timestamp:
+            if values[m][1] <= timestamp:
                 res = values[m][0]
                 l = m+1
             else:
