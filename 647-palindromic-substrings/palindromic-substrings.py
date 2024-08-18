10ffde6b737c609f16ba751442909a88ddc48f4f
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-17 19:51:33 -0500

    Time: 169 ms (46.40%) | Memory: 16.6 MB (47.43%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,12 +1,48 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        self.res = 0
-        def exploreFromMiddle(l,r):
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                self.res += 1
-                l -= 1
-                r += 1
+        pallindromes = 0
+        def checkFromMiddle(l,r):
+            count = 0
+            while l >= 0 and r <= len(s)-1:
+                if s[l] == s[r]:
+                    count += 1
+                    l -= 1
+                    r += 1
+                else:
+                    break
+            return count
+
         for i in range(len(s)):
-            exploreFromMiddle(i,i)
-            exploreFromMiddle(i,i+1)
-        return self.res
+            pallindromes += checkFromMiddle(i, i)  # the length is odd
+            pallindromes += checkFromMiddle(i, i+1) # the length is odd
+        return pallindromes
+
+
+
+        # abcacba
+        # a, b, c, a, cac, bcacb, abcacba, c, b, a length is odd
+        # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # self.res = 0
+        # def exploreFromMiddle(l,r):
+        #     while l >= 0 and r < len(s) and s[l] == s[r]:
+        #         self.res += 1
+        #         l -= 1
+        #         r += 1
+        # for i in range(len(s)):
+        #     exploreFromMiddle(i,i)
+        #     exploreFromMiddle(i,i+1)
+        # return self.res
