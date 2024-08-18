19b6e561fb300b47ee678efd282ddadddc4edbf2
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-17 21:08:44 -0500

    Time: 1097 ms (18.95%) | Memory: 44.6 MB (8.48%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,21 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res = float('inf')
         cache = {}
         def helper(cur_amount):
             if cur_amount == 0:
                 return 0
             if cur_amount < 0:
-                return float('inf')
+                return amount+1
             if cur_amount in cache:
                 return cache[cur_amount]
-            res = float('inf')
+            res = amount+1
             for coin in coins:
                 res = min(res,helper(cur_amount-coin)+1)
             cache[cur_amount] = res
             return cache[cur_amount]
         res = helper(amount)
-        return res if res != float('inf') else -1
+        return res if res != amount+1 else -1
 
 
 
@@ -27,10 +26,10 @@ class Solution:
 
 
 
-        # dp = [amount+1]*(amount+1)
-        # dp[0] = 0
-        # for a in range(1,amount+1):
-        #     for coin in coins:
-        #         if a - coin >= 0:
-        #             dp[a] = min(dp[a],1+dp[a-coin])
-        # return dp[amount] if dp[amount] != amount+1 else -1
+        dp = [amount+1]*(amount+1)
+        dp[0] = 0
+        for a in range(1,amount+1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a],1+dp[a-coin])
+        return dp[amount] if dp[amount] != amount+1 else -1
