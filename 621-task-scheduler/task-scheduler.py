0b5a34c9e8ed1d0311600c085f1c605e4c56b117
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-19 10:30:29 -0500

    Time: 481 ms (34.84%) | Memory: 16.9 MB (82.54%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -3,16 +3,16 @@ class Solution:
         taskDict = Counter(tasks)
         maxHeap = [-value for key,value in taskDict.items()]
         heapq.heapify(maxHeap)
-        t = 0
+        cur_t = 0
         q = deque()
         while maxHeap or q:
-            t += 1
+            cur_t += 1
             if maxHeap:
                 count = heapq.heappop(maxHeap)
                 count += 1
                 if count != 0:
-                    q.append((count,t+n))
-            if q and q[0][1] == t:
+                    q.append((count, cur_t+n))
+            if q and q[0][1] == cur_t:
                 count, _ = q.popleft()
                 heapq.heappush(maxHeap,count)
-        return t
+        return cur_t
