163fa0b002a68177c3d00051a12e39b436c92f0f
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-19 00:12:45 -0500

    Time: 33 ms (83.30%) | Memory: 16.7 MB (76.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,26 +2,15 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         length = n*2
         res = []
-        def valid(s):
-            stack = []
-            for char in s:
-                if char == '(':
-                    stack.append(char)
-                else:
-                    if stack:
-                        stack.pop()
-                    else:
-                        return False
-            return len(stack) == 0
-        def helper(s):
+        def helper(s, left, right):
             if len(s) == length:
                 res.append(s)
                 return
-            if s.count('(') < n:
-                helper(s + '(')
-            if s.count(')') < s.count('('):
-                helper(s + ')')
-        helper("")
+            if left < n:
+                helper(s+"(", left+1,right)
+            if right < left:
+                helper(s+")", left, right+1)
+        helper("",0,0)
         return res
         # length = n*2
         # res = set()
