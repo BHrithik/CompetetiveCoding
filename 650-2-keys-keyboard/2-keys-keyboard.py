3907a107bc4cf909ce2313e184fadf8b3ce84f43
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-18 23:01:04 -0500

    Time: 753 ms (5.12%) | Memory: 16.7 MB (36.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -4,15 +4,14 @@ class Solution:
             return 0
         if n == 2:
             return n
-        res = float('inf')
         desired = "A"*n
         cache = {}
         def dfs(x, clipBoard, count):
-            if x == desired:
+            if x == n:
                 return count
-            if len(x) > n:
+            if x > n:
                 return float('inf')
             # n is even and n > 2
             # 2 options every time copy existing or paste from clipboard
             return min(dfs(x+clipBoard,clipBoard,count+1), dfs(x+x,x,count+2))
-        return dfs("A","A",1)
+        return dfs(1,1,1)
