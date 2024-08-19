8b263eaf8202d4f64702baccc48642b2cf81c648
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-19 10:39:50 -0500

    Time: 408 ms (25.42%) | Memory: 38.9 MB (11.05%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -5,14 +5,14 @@ class MedianFinder:
         self.maxHeap = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.maxHeap,-num)
-        heapq.heappush(self.minHeap,-(heapq.heappop(self.maxHeap)))
-        if len(self.minHeap) > len(self.maxHeap)+1:
+        heapq.heappush(self.maxHeap,-num) # initially add the number to maxHeap
+        heapq.heappush(self.minHeap,-(heapq.heappop(self.maxHeap))) # remove the largest number from maxheap and add it to min heap
+        if len(self.minHeap) > len(self.maxHeap)+1: # if length difference is more than 1 remove element from min heap and add it to maxHeap
             heapq.heappush(self.maxHeap,-heapq.heappop(self.minHeap))
 
     def findMedian(self) -> float:
-        if len(self.minHeap)>len(self.maxHeap):
+        if len(self.minHeap)>len(self.maxHeap): # since min heap will always have more elements we can return the top of the heap as median
             return self.minHeap[0]
-        else:
+        else: # since the count is even we need to get the top of 2 heaps and divide them with 2 to return min and max
             return (self.minHeap[0]-self.maxHeap[0])/2
         
