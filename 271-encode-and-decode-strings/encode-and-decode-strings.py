615a19503bf47dded9684f38dc93e314d4f9b2dc
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-18 22:28:43 -0500

    Time: 61 ms (86.89%) | Memory: 16.9 MB (48.09%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -7,22 +7,23 @@ class Codec:
             s += str(len(i))+"#"+i
         print(s)
         return s
-        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res,i = [],0
-        while i<len(s):
-            j = i
-            while s[j]!="#":
-                j += 1
-            num = int(s[i:j])
-            j = j+1 ## first letter
-            i = num+j ## start of next letter
-            res.append(s[j:i])
+        res = []
+        l = 0
+        r = 0 
+        while r<len(s):
+            if s[r] == "#":
+                len_wrd = int(s[l:r])
+                word = s[r+1:r+1+(len_wrd)]
+                res.append(word)
+                r = r+len_wrd+1
+                l = r
+            r += 1
         return res
-
+        
 
         
 
