3c4c28ec78f1824801a2a4179e4dc4d977b2ad15
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-19 08:59:08 -0500

    Time: 43 ms (39.21%) | Memory: 16.8 MB (29.38%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,12 +1,20 @@
+from typing import List
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]]        
-        perms = self.permute(nums[1:])
         res = []
-        for p in perms:
-            for i in range(len(p)+1):
-                p_copy = p.copy()
-                p_copy.insert(i,nums[0])
-                res.append(p_copy)
+        subSet = []
+        visited = set()
+        def dfs():
+            if len(subSet) == len(nums):
+                res.append(subSet.copy())
+                return
+            for i in range(len(nums)):
+                if nums[i] not in visited:
+                    subSet.append(nums[i])
+                    visited.add(nums[i])
+                    dfs()
+                    subSet.pop()
+                    visited.remove(nums[i])
+        dfs()
         return res
