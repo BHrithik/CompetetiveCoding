f9d111949f9e220ec917d2b9dcfffe0c14fb9766
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-08-30 14:49:28 -0500

    Time: 36 ms (45.98%) | Memory: 16.4 MB (95.13%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,26 +1,67 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        if '[' not in s:
-            return s
-        index = s.find('[')
-        number = ''
-        i = index - 1
-        while i >= 0 and s[i].isdigit():
-            number = s[i] + number
-            i -= 1
-        num_start_index = i + 1
-        brackets = 1  # Start with 1 as we are already at one open bracket
-        start = index
-        for i in range(index + 1, len(s)):
-            if s[i] == '[':
-                brackets += 1
-            elif s[i] == ']':
-                brackets -= 1
-                if brackets == 0:
-                    end = i
-                    break
-        new_string = ''
-        for i in range(int(number)):
-            new_string += s[start+1:end]
+        stack = []
+        for i in s:
+            if i== ']':
+                cur_s = ""
+                while stack:
+                    ch = stack.pop()
+                    if ch == "[":
+                        break
+                    cur_s += ch
+                cur_digit = ""
+                while stack:
+                    di = stack.pop()
+                    if di.isdigit():
+                        cur_digit += di
+                    else:
+                        stack.append(di)
+                        break
+                cur_digit = cur_digit[::-1]
+                if not cur_digit:
+                    cur_digit = 1
+                else:
+                    cur_digit = int(cur_digit)
+                stack.append(cur_s*cur_digit)
+            else:
+                stack.append(i)
+        res = ""
+        for i in stack:
+            res += i[::-1]
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+        # if '[' not in s:
+        #     return s
+        # index = s.find('[')
+        # number = ''
+        # i = index - 1
+        # while i >= 0 and s[i].isdigit():
+        #     number = s[i] + number
+        #     i -= 1
+        # num_start_index = i + 1
+        # brackets = 1  # Start with 1 as we are already at one open bracket
+        # start = index
+        # for i in range(index + 1, len(s)):
+        #     if s[i] == '[':
+        #         brackets += 1
+        #     elif s[i] == ']':
+        #         brackets -= 1
+        #         if brackets == 0:
+        #             end = i
+        #             break
+        # new_string = ''
+        # for i in range(int(number)):
+        #     new_string += s[start+1:end]
         
-        return self.decodeString(s[:num_start_index] + new_string + s[end+1:])
+        # return self.decodeString(s[:num_start_index] + new_string + s[end+1:])
