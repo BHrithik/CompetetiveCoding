738cf50bb5065444089a575709fd8dda80548f89
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-01 12:08:22 -0500

    Time: 688 ms (43.60%) | Memory: 23.9 MB (55.74%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -3,11 +3,9 @@ class Solution:
         if m*n != len(original):
             return []
         res = [[0]*n for _ in range(m)]
-        print(res)
         c = 0
         for i in range(len(res)):
             for j in range(len(res[i])):
                 res[i][j] = original[c]
                 c += 1
-        print(res)
         return res
