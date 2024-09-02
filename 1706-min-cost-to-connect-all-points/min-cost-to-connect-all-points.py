b49215be07aa600ca39dab46c45b486bcce3f266
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-01 22:18:18 -0500

    Time: 1183 ms (64.49%) | Memory: 154.4 MB (34.43%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        adjList = {i:[] for i in range(n)}
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i+1,n):
+                x2, y2 = points[j]
+                dist = abs(x2-x1) + abs(y2-y1)
+                adjList[i].append((dist,j))
+                adjList[j].append((dist,i))
+        
+        # Prim's ( Or as I like to call it Prem's)
+        res = 0
+        visited = set()
+        mH = [[0,0]]
+        heapq.heapify(mH)
+        while len(visited) < n:
+            cost, i = heapq.heappop(mH)
+            if i in visited:
+                continue
+            res += cost
+            visited.add(i)
+            for neiDist, nei in adjList[i]:
+                if nei not in visited:
+                    heapq.heappush(mH, (neiDist, nei))
+        return res
