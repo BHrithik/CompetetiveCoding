3825e46623f93aa7737a5ce7543570c367e1360b
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-03 01:30:44 -0500

    Time: 92 ms (47.53%) | Memory: 18.3 MB (80.36%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,25 +1,60 @@
 class Solution:
     def findOrder(self, numCourses: int, preq: List[List[int]]) -> List[int]:
-        courseDict = {i:[] for i in range(numCourses)}
-        for c1,c2 in preq:
-            courseDict[c1].append(c2)
-        visited = set()
-        cycle = set()
-        output = []
-        def dfs(c):
-            if c in cycle:
+        graph = {i:[] for i in range(numCourses)}
+        for a,b in preq:
+            graph[a].append(b)
+        visited = [0]*numCourses # 0 is not visited 1 is visiting 2 is visited
+        outPut = []
+        def isCycle(i):
+            if visited[i] == 2:
                 return False
-            if c in visited:
+            if visited[i] == 1:
                 return True
-            cycle.add(c)
-            visited.add(c)
-            for pre in courseDict[c]:
-                if not dfs(pre):
-                    return False
-            cycle.remove(c)
-            output.append(c)
-            return True
+            visited[i] = 1 # Visiting
+            for pres in graph[i]:
+                if isCycle(pres):
+                    return True
+            visited[i] = 2
+            outPut.append(i)
+            return False
+
         for i in range(numCourses):
-            if not dfs(i):
+            if visited[i] != 2 and isCycle(i):
                 return []
-        return output
+        return outPut
+
+
+
+
+
+
+
+
+
+
+
+
+        # courseDict = {i:[] for i in range(numCourses)}
+        # for c1,c2 in preq:
+        #     courseDict[c1].append(c2)
+        # visited = set()
+        # cycle = set()
+        # output = []
+        # def dfs(c):
+        #     if c in cycle:
+        #         return False
+        #     if c in visited:
+        #         return True
+        #     cycle.add(c)
+        #     visited.add(c)
+        #     for pre in courseDict[c]:
+        #         if not dfs(pre):
+        #             return False
+        #     cycle.remove(c)
+        #     output.append(c)
+        #     return True
+        # for i in range(numCourses):
+        #     if not dfs(i):
+        #         print(output)
+        #         return []
+        # return output
