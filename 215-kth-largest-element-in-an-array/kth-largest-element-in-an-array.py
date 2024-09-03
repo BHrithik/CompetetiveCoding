a009253203d6ff0831b83dd6ded723151afa935a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-03 02:00:51 -0500

    Time: 493 ms (30.63%) | Memory: 28.8 MB (95.76%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,20 +1,20 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         # Quick Select
-        def quickSelect(l,r):
-            pivot = nums[r]
-            i = l
-            for j in range(l,r):
-                if nums[j] <= pivot:
-                    nums[i],nums[j] = nums[j],nums[i]
-                    i+=1
-            nums[r], nums[i] = nums[i], nums[r]
-            if len(nums)-i == k:
-                return nums[i]
-            elif len(nums)-i > k:
-                return quickSelect(i+1,r)
-            else:
-                return quickSelect(l,i-1)
+        # def quickSelect(l,r):
+        #     pivot = nums[r]
+        #     i = l
+        #     for j in range(l,r):
+        #         if nums[j] <= pivot:
+        #             nums[i],nums[j] = nums[j],nums[i]
+        #             i+=1
+        #     nums[r], nums[i] = nums[i], nums[r]
+        #     if len(nums)-i == k:
+        #         return nums[i]
+        #     elif len(nums)-i > k:
+        #         return quickSelect(i+1,r)
+        #     else:
+        #         return quickSelect(l,i-1)
         # return quickSelect(0,len(nums)-1)
 
         minHeap = nums
