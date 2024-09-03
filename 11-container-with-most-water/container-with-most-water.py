68a891e864415eb3dc203b758ce3b3e1fb05c501
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-02 20:20:29 -0500

    Time: 529 ms (39.62%) | Memory: 29.8 MB (26.02%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,25 +1,57 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        maxArea = 0
-        H = max(height)
-
-        while left < right:
-            W = (right - left)
-
-            if H*W <= maxArea:
-                return maxArea
-            
-            currentArea = min(height[left], height[right]) * W
-            maxArea = max(maxArea, currentArea)
-            
-            if height[left] == height[right]:
-                left += 1
-                right -= 1
-            elif height[left] < height[right]:
-                left += 1
+        l = 0
+        r = len(height)-1
+        maxWater = 0
+        while l < r:
+            h = min(height[l], height[r])
+            maxWater = max(maxWater, h* (r-l))
+            if height[l] > height[r]:
+                r -= 1
             else:
-                right -= 1
+                l += 1
+        return maxWater
 
-        return maxArea
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # left = 0
+        # right = len(height) - 1
+        # maxArea = 0
+        # H = max(height)
+
+        # while left < right:
+        #     W = (right - left)
+
+        #     if H*W <= maxArea:
+        #         return maxArea
+            
+        #     currentArea = min(height[left], height[right]) * W
+        #     maxArea = max(maxArea, currentArea)
+            
+        #     if height[left] == height[right]:
+        #         left += 1
+        #         right -= 1
+        #     elif height[left] < height[right]:
+        #         left += 1
+        #     else:
+        #         right -= 1
+
+        # return maxArea
