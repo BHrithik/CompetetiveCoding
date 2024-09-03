39c23cca070f9ca1b023c93ea060eb5946e8debd
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-02 20:40:42 -0500

    Time: 1736 ms (9.19%) | Memory: 20 MB (95.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,19 +1,54 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         res = set()
         nums.sort()
-        for i in range(0,len(nums)):
+        for i in range(n):
             target = -nums[i]
             l = i+1
-            r = len(nums)-1
+            r = n-1
             while l < r:
-                cur_sum = nums[l]+nums[r]
-                if cur_sum == target:
-                    res.add(tuple([nums[i],nums[l],nums[r]]))
+                if nums[l] + nums[r] == target:
+                    if i != l and i != r:
+                        res.add(tuple([nums[i],nums[l],nums[r]]))
                     l += 1
                     r -= 1
-                elif cur_sum < target:
-                    l += 1
+                elif nums[l]+nums[r] > target:
+                    r -= 1
                 else:
-                    r -= 1
+                    l += 1
         return list(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = set()
+        # nums.sort()
+        # for i in range(0,len(nums)):
+        #     target = -nums[i]
+        #     l = i+1
+        #     r = len(nums)-1
+        #     while l < r:
+        #         cur_sum = nums[l]+nums[r]
+        #         if cur_sum == target:
+        #             res.add(tuple([nums[i],nums[l],nums[r]]))
+        #             l += 1
+        #             r -= 1
+        #         elif cur_sum < target:
+        #             l += 1
+        #         else:
+        #             r -= 1
+        # return list(res)
