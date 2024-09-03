6e4f43c9b1b881fe1779b7c190fae40ca10922a7
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-03 00:46:58 -0500

    Time: 1672 ms (12.98%) | Memory: 61.6 MB (80.10%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -3,11 +3,11 @@ class Solution:
         good = set()
         for t in triplets:
             skip = False
-            for n in range(len(target)):
+            for n in range(3):
                 if t[n] > target[n]:
                     skip = True
             if not skip:
-                for i in range(len(t)):
+                for i in range(3):
                     if t[i] == target[i]:
                         good.add(i)
         return len(good) == len(target)
