8e62562efa9c1633db4357e13e530c5f10190ebd
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-05 17:20:20 -0500

    Time: 36 ms (52.60%) | Memory: 16.5 MB (70.01%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-
-        count = collections.Counter({'a':a,'b':b,'c':c})
-        res = ['#']
-        
-        while True:
-            (a1, _), (a2, _) = count.most_common(2)
-
-            if a1 == res[-1] == res[-2]:
+        res = "#"
+        count = Counter({'a':a, 'b':b, 'c':c})
+        # chars = sorted(count.values(),key=lambda x: count[x])
+        while count:
+            (a1, _),(a2, _) = count.most_common(2)
+            if len(res) > 2 and res[-1] == res[-2] == a1:
                 a1 = a2
-            
             if not count[a1]:
                 break
-            
-            res.append(a1)
+            res+= a1
             count[a1] -= 1
-
-        return ''.join(res[1:])
+        return res[1:]
