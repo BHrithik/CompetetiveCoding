ebedc2c1d9b737d75287c106aec24358193a9dbc
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-05 17:25:16 -0500

    Time: 93 ms (52.25%) | Memory: 16.6 MB (52.89%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        boxes = defaultdict(set)
-        cols = defaultdict(set)
-        rows = defaultdict(set)
-        n = len(board)
-        for i in range(n):
-            for j in range(n):
+        cols = defaultdict(lambda: set())
+        rows = defaultdict(lambda: set())
+        boxes = defaultdict(lambda: set())
+        for i in range(len(board)):
+            for j in range(len(board)):
                 if board[i][j] == ".":
                     continue
-                box_n = ((i//3)*3,(j//3)*3)
-                if board[i][j] in cols[j] or board[i][j] in rows[i] or board[i][j] in boxes[box_n]:
+                box = ((i//3)*3, (j//3)*3) # To caculate which box the current number lies in 
+                if board[i][j] in cols[i] or board[i][j] in rows[j] or board[i][j] in boxes[box]:
                     return False
-                cols[j].add(board[i][j])
-                rows[i].add(board[i][j])
-                boxes[box_n].add(board[i][j])
+                cols[i].add(board[i][j])
+                rows[j].add(board[i][j])
+                boxes[box].add(board[i][j])
         return True
-    
+                
