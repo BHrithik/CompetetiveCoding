805fc044a7a2d18fe386957462bf9a70d18c6a4a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-06 01:18:19 -0500

    Time: 480 ms (34.86%) | Memory: 30.8 MB (32.32%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,14 +1,18 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, 0
-        while True:
+        # First observation since nums is of size n and values can be from [1,n]
+        # We should look at them like a linked list where each value in the array is 
+        # pointer to the next one
+        slow, fast = 0, 0 # We know that the first element is not the duplicate as there is no 0, The elements start at 1
+        while True: # Find the first point of intersection with fast and slow pointers.
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
-        slow2 = 0
+        slow2 = 0 # Start a new slow pointer from the beginning 
+        # and according to Floyd's algorithm it should reach the point of intersection at the same time as fast
         while True:
-            slow = nums[slow]
+            fast = nums[fast]
             slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
+            if fast == slow2:
+                return fast
