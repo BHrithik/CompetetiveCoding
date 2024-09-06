c75bcecaaf3d226c507441ef65134f44aab2d708
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-06 02:03:36 -0500

    Time: 566 ms (63.84%) | Memory: 78.5 MB (36.93%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,49 +1,53 @@
 class Node:
-    def __init__(self,key=0,value=0):
+    def __init__(self, key, val):
         self.key = key
-        self.value= value
-        self.next,self.prev = None, None
+        self.val = val
+        self.prev = self.next = None
+
+
 
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cap = capacity
         self.cache = {}
-        self.left, self.right = Node(),Node()
+        self.cap = capacity
+        self.left = Node(0,0)
+        self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
 
-    # Inserting a node at the most frequent node i.e. the right
     def insert(self,node):
-        prev_node = self.right.prev
-        prev_node.next = node
-        node.prev = prev_node
         node.next = self.right
+        temp = self.right.prev
+        temp.next = node
         self.right.prev = node
-
-
-    # Removing the left most value in our doubly linked link list
+        node.prev = temp
+    
     def remove(self,node):
-        front = node.next
-        back = node.prev
-        back.next = front
-        front.prev = back
+        prevNode = node.prev
+        nextNode = node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key]) # remove it from the current chain
-            self.insert(self.cache[key]) # add it to the left most as it is the most recently used
-            return self.cache[key].value
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])  # Remove the old node
-        new_node = Node(key, value)
+            node = self.cache[key]
+            self.remove(node)
+        else:
+            if len(self.cache) == self.cap:
+                lru = self.left.next
+                self.remove(lru)
+                del self.cache[lru.key]
+        new_node = Node(key,value)
         self.insert(new_node)
         self.cache[key] = new_node
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]     
+                
