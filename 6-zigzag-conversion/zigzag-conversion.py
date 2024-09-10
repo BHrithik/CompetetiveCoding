1abcb35633c79c3c3b341a73e166e3960688d453
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-09 20:41:34 -0500

    Time: 50 ms (64.54%) | Memory: 16.7 MB (30.30%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -2,14 +2,14 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        res = [[""] for _ in range(numRows)]
+        res = ["" for _ in range(numRows)]
         level = 0
         sign = 1
         for i in s:
-            res[level][0] += i
-            if level == numRows-1:
-                sign = -1
-            if level == 0:
-                sign = 1
+            res[level] += i
+            if level == numRows-1: # Going up once we reached the bottom
+                sign = -1 # Going up
+            if level == 0: # Going down once we reached the top
+                sign = 1 # Going Down
             level += sign
-        return "".join([i[0] for i in res])
+        return "".join([i for i in res])
