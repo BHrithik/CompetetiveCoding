e3b84cccd85d0de59736b3873e18e0a2722bb364
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-10 11:08:52 -0500

    Time: 79 ms (11.58%) | Memory: 16.5 MB (93.63%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -2,18 +2,49 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        subSet = []
-        def bcktrk(i,cur_sum):
-            if cur_sum == target:
-                res.append(subSet.copy())
+        combination = []
+        def dfs(i, a):
+            if a == target:
+                res.append((combination.copy()))
                 return
-            if cur_sum > target or i>= len(candidates):
-                return
-            subSet.append(candidates[i])
-            bcktrk(i+1,cur_sum+candidates[i])
-            subSet.pop()
-            while i+1 < len(candidates) and candidates[i+1] == candidates[i]:
-                i = i+1
-            bcktrk(i+1, cur_sum)
-        bcktrk(0,0)
+            if i > len(candidates)-1 or a > target: return
+            combination.append(candidates[i])
+            dfs(i+1,a+candidates[i])
+            combination.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            dfs(i+1,a)
+        dfs(0,0)
         return list(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # candidates.sort()
+        # res = []
+        # subSet = []
+        # def bcktrk(i,cur_sum):
+        #     if cur_sum == target:
+        #         res.append(subSet.copy())
+        #         return
+        #     if cur_sum > target or i>= len(candidates):
+        #         return
+        #     subSet.append(candidates[i])
+        #     bcktrk(i+1,cur_sum+candidates[i])
+        #     subSet.pop()
+        #     while i+1 < len(candidates) and candidates[i+1] == candidates[i]:
+        #         i = i+1
+        #     bcktrk(i+1, cur_sum)
+        # bcktrk(0,0)
+        # return list(res)
