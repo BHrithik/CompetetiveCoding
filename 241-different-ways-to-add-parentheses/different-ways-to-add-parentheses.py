6c24f3c709367b6d25d8713a314966186746b5d3
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 15:30:25 -0500

    Time: 27 ms (97.18%) | Memory: 16.5 MB (90.55%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,11 +1,22 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
+        # res = []
+        # def helper(left,right):
+        #     sub_exp = expression[left:right+1]
+        #     if '-' not in sub_exp and '+' not in sub_exp and '*' not in sub_exp:
+        #         return int(sub_exp)
+            
+
+
         operations = {
             '+':lambda x,y : x+y,
             '-':lambda x,y : x-y,
             '*':lambda x,y : x*y
         }
         def backtrack(left,right):
+            sub_exp = expression[left:right+1]
+            if '-' not in sub_exp and '+' not in sub_exp and '*' not in sub_exp:
+                return [int(sub_exp)]
             res = []
             for i in range(left, right+1):
                 if expression[i] in operations:
@@ -14,7 +25,7 @@ class Solution:
                     for n in nums1:
                         for n2 in nums2:
                             res.append(operations[expression[i]](n,n2))
-            if res == []:
-                res.append(int(expression[left:right+1]))
+            # if res == []:
+            #     res.append(int(expression[left:right+1]))
             return res  
         return backtrack(0,len(expression)-1)
