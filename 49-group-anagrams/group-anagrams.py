ee1bb9802d97adcd500ccbec3628112668ae6945
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 17:51:43 -0500

    Time: 88 ms (65.71%) | Memory: 20.7 MB (40.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        new_strs = {}
-        for index,value in enumerate(strs):
-            string = "".join(sorted(value))
-            if string not in new_strs:
-                new_strs[string] = []
-            new_strs[string].append(value)
-        return new_strs.values()
+        anagrams = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            if key in anagrams:
+                anagrams[key].append(s)
+            else:
+                anagrams[key] = [s]
+        return anagrams.values()
