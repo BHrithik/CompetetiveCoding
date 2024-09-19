8c290c9746654847a2bebf259c3609dc08029244
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 15:21:47 -0500

    Time: 29 ms (94.82%) | Memory: 16.6 MB (67.91%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,19 +1,19 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
+        operations = {
+            '+':lambda x,y : x+y,
+            '-':lambda x,y : x-y,
+            '*':lambda x,y : x*y
+        }
         def backtrack(left,right):
             res = []
             for i in range(left, right+1):
-                if expression[i] in "+-*":
+                if expression[i] in operations:
                     nums1 = backtrack(left,i-1)
                     nums2 = backtrack(i+1,right)
                     for n in nums1:
                         for n2 in nums2:
-                            if expression[i] == "+":
-                                res.append(n+n2)
-                            elif expression[i] == "-":
-                                res.append(n-n2)
-                            elif expression[i] == "*":
-                                res.append(n*n2)
+                            res.append(operations[expression[i]](n,n2))
             if res == []:
                 res.append(int(expression[left:right+1]))
             return res  
