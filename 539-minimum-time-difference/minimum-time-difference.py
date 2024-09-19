0c32b0acb1ee4834e6126d09177ade0454a55ba5
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 17:03:44 -0500

    Time: 106 ms (5.49%) | Memory: 19.8 MB (8.92%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -4,9 +4,9 @@ class Solution:
             nums = time.split(":")
             return int(nums[0])*60 + int(nums[1])
         
-        nums = [timeToNum(time) for time in timePoints]
-        nums.sort()
-        minSum = (1440-nums[-1])+nums[0]
-        for i in range(0,len(nums)-1):
-            minSum = min(minSum, abs(nums[i-1]-nums[i]), abs(nums[i+1]-nums[i]))
+        # nums = [timeToNum(time) for time in timePoints]
+        timePoints.sort(key=lambda x: timeToNum(x))
+        minSum = (1440-timeToNum(timePoints[-1]))+timeToNum(timePoints[0])
+        for i in range(0,len(timePoints)-1):
+            minSum = min(minSum, abs(timeToNum(timePoints[i-1])-timeToNum(timePoints[i])), abs(timeToNum(timePoints[i+1])-timeToNum(timePoints[i])))
         return minSum
