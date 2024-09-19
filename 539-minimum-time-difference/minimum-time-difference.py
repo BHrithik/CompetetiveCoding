14cf55135a704eb4c3737b2869b9546c77b09866
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 18:48:22 -0500

    Time: 60 ms (92.59%) | Memory: 19.6 MB (72.71%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -2,11 +2,21 @@ class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
         def timeToNum(time):
             nums = time.split(":")
-            return int(nums[0])*60 + int(nums[1])
-        
-        nums = [timeToNum(time) for time in timePoints]
-        nums.sort()
-        minSum = (1440-nums[-1])+nums[0]
-        for i in range(0,len(nums)-1):
-            minSum = min(minSum, abs(nums[i-1]-nums[i]), abs(nums[i+1]-nums[i]))
-        return minSum
+            return int(nums[0])*60 + int(nums[1])        
+        bucket = [False]*1440
+        first_t = 1441
+        last_t = 0
+        for time in timePoints:
+            mins = timeToNum(time)
+            if bucket[mins]:
+                return 0
+            bucket[mins] = True
+            first_t = min(first_t,mins)
+            last_t = max(last_t,mins)
+        min_diff = 1440-last_t+first_t
+        prev = first_t
+        for i in range(first_t+1,len(bucket)):
+            if bucket[i]:
+                min_diff = min(min_diff, i-prev)
+                prev = i
+        return min_diff
