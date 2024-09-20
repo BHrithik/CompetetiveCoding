45399cfc8ac67b8898d7724dd1b2b8d5a5b1c7b8
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 20:06:15 -0500

    Time: 41 ms (13.19%) | Memory: 16.5 MB (72.71%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,29 +1,15 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        nums1 = version1.split('.')
-        nums2 = version2.split('.')
-        nums1 = [int(i) for i in nums1]
-        nums2 = [int(i) for i in nums2]
-        for i in range(len(nums1)-1,-1,-1):
-            if nums1[i] != 0:
-                break
-        nums1 = nums1[:i+1]
-        for i in range(len(nums2)-1,-1,-1):
-            if nums2[i] != 0:
-                break
-        nums2 = nums2[:i+1]
-        c = 0
-        while c < min(len(nums1),len(nums2)):
-            if nums1[c] == nums2[c]:
-                c += 1
-                continue
-            elif nums1[c] < nums2[c]:
-                return -1
-            else:
+        nums1 = [int(i) for i in version1.split('.')]
+        nums2 = [int(i) for i in version2.split('.')]
+        
+        # Compare the two versions number by number
+        for i in range(max(len(nums1), len(nums2))):
+            v1 = nums1[i] if i < len(nums1) else 0
+            v2 = nums2[i] if i < len(nums2) else 0
+            if v1 > v2:
                 return 1
-        if len(nums1) == len(nums2):
-            return 0
-        if c >= len(nums1):
-            return -1
-        else:
-            return 1
+            elif v1 < v2:
+                return -1
+        
+        return 0
