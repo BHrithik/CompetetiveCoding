3ad897478f5df5e79a86b0f90269f8dc3ab2c8d7
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-20 10:20:13 -0500

    Time: 44 ms (50.49%) | Memory: 16.8 MB (71.58%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,18 +6,14 @@ class Solution:
             m = (l+r)//2
             if nums[m] == target:
                 return m
-            #left sorted array
             if nums[l] <= nums[m]:
-                if target > nums[m] or nums[l] > target:
+                if target > nums[m] or target < nums[l]:
                     l = m+1
                 else:
                     r = m-1
-            else: ## right sorted array
-                if target < nums[m] or nums[r] < target:
+            else:
+                if target < nums[m] or target > nums[r]:
                     r = m-1
                 else:
                     l = m+1
         return -1
-            
-#  [4,5,6,7,0,1,2]
-#   l     m     r                 
