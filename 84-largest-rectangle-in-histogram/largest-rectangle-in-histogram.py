28c791be328f474b747cc32a6002675e3847dcc8
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 20:09:32 -0500

    Time: 731 ms (16.33%) | Memory: 33.4 MB (42.34%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -3,11 +3,13 @@ class Solution:
         heights.append(0)
         stack = []
         res = 0
+        l = 0
         for i in range(0,len(heights)):
             height = heights[i]
             while stack and stack[-1][0] > height:
                 h, _ = stack.pop()
-                width = i if not stack else i - stack[-1][1] - 1
+                width = i if not stack else i - stack[-1][1] -1
                 res = max(res,h*width)
+                l += 1
             stack.append((height,i))
         return res
