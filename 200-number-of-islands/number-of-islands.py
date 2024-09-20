ce9aa0c308d3b08a0d0c22e777c7c9e94c46857a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-20 01:00:55 -0500

    Time: 235 ms (73.92%) | Memory: 19 MB (60.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,7 +2,6 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
         directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        visited = set()
         def helper(i,j):
             if not 0 <= i < len(grid) or not 0 <= j < len(grid[i]) or grid[i][j] == "0":
                 return
@@ -12,7 +11,7 @@ class Solution:
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] == "1" and (i,j) not in visited:
+                if grid[i][j] == "1":
                     helper(i,j)
                     res += 1
         return res
