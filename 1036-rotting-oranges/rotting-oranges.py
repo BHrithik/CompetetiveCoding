6e21ec003ed5ab0a40b7f24dcc0115ac35846fa0
Hrithik <31255192+BHrithik@users.noreply.github.com>
2024-09-19 20:51:48 -0500

    Time: 43 ms (91.19%) | Memory: 16.8 MB (17.26%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,9 +1,8 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        def countFreshAndRotten(grid): # Big O (n*m)
-            count =0
-            rotten = []
+        def countFreshAndRotten(grid): # Big O is (n*m)
+            count, rotten = 0,[]
             for i in range(0,len(grid)):
                 for j in range(0,len(grid[0])):
                     if grid[i][j] == 1:
@@ -11,7 +10,7 @@ class Solution:
                     if grid[i][j] == 2:
                         rotten.append((i,j))
             return count, rotten
-        def rot(rotten,fresh):  # Big always less than (o*m)
+        def rot(rotten,fresh):  # Big O is always less than (o*m)
             new_rotten = []
             for i,j in rotten:
                 for di, dj in directions:
@@ -21,7 +20,6 @@ class Solution:
                         new_rotten.append((ni,nj))
                         fresh -= 1
             return new_rotten, fresh
-        prevGrid = []
         time = 0
         fresh,rotten = countFreshAndRotten(grid)
         while rotten and fresh > 0:
