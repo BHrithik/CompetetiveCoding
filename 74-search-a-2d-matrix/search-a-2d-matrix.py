cd8850e1051b23de737b7d4ee80cfc04a073643a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-11 01:33:59 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,26 +1,26 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        l = 0
-        r = len(matrix)-1
-        row = 0
-        while l <= r:
-            m = (l+r)//2
-            if matrix[m][0] <= target <= matrix[m][-1]:
-                row = m
-                break
-            elif target < matrix[m][0]:
-                r = m-1
+        def binary_search(arr, target):
+            l = 0
+            r = len(arr)-1
+            while l <= r:
+                m = (l+r) // 2
+                if arr[m] == target:
+                    return True
+                if arr[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return False
+
+        l_row = 0
+        r_row = len(matrix)-1
+        while l_row <= r_row:
+            m_row = (l_row+r_row)//2
+            if matrix[m_row][0] <= target <= matrix[m_row][-1]:
+                return binary_search(matrix[m_row], target)
+            if matrix[m_row][0] > target:
+                r_row = m_row - 1
             else:
-                l = m+1
-        arr = matrix[row]
-        l = 0
-        r = len(matrix[row]) -1
-        while l <= r:
-            m = (l+r)//2
-            if matrix[row][m] == target:
-                return True
-            elif matrix[row][m] > target:
-                r = m-1
-            else:
-                l = m+1
+                l_row = m_row + 1
         return False
