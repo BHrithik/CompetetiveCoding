21d94d67d887994e533f2d22801f0a43447c69ce
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-11 00:21:54 -0800

    Time: 63 ms (88.97%) | Memory: 29.6 MB (6.25%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,54 +4,10 @@ class Solution:
         r = len(height)-1
         maxWater = 0
         while l < r:
-            h = min(height[l], height[r])
-            maxWater = max(maxWater, h* (r-l))
-            if height[l] > height[r]:
-                r -= 1
-            else:
+            cur_water = min(height[l],height[r])*(r-l)
+            if height[l] < height[r]:
                 l += 1
+            else:
+                r -= 1
+            maxWater = max(maxWater, cur_water)
         return maxWater
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # left = 0
-        # right = len(height) - 1
-        # maxArea = 0
-        # H = max(height)
-
-        # while left < right:
-        #     W = (right - left)
-
-        #     if H*W <= maxArea:
-        #         return maxArea
-            
-        #     currentArea = min(height[left], height[right]) * W
-        #     maxArea = max(maxArea, currentArea)
-            
-        #     if height[left] == height[right]:
-        #         left += 1
-        #         right -= 1
-        #     elif height[left] < height[right]:
-        #         left += 1
-        #     else:
-        #         right -= 1
-
-        # return maxArea
