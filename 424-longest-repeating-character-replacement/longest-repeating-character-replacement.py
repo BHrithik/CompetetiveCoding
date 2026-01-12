44b62bffba7ee75c6c2a0deabaef09a8de3b45af
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-12 12:54:56 -0800

    Time: 77 ms (95.79%) | Memory: 19.6 MB (11.22%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,9 +3,11 @@ class Solution:
         l = 0
         char_count = {}
         res = 0
+        maxF = 0
         for r in range(len(s)):
             char_count[s[r]] = char_count.get(s[r],0) + 1
-            if r-l+1 - max(char_count.values()) > k:
+            maxF = max(char_count[s[r]],maxF)
+            if r-l+1 - maxF > k:
                 char_count[s[l]] -= 1
                 l += 1
             res = max(res, r-l+1)
