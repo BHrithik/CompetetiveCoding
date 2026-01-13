867cac40f523357bc6040e51ee549b6ff3b04b7e
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 00:39:57 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.33%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,15 +6,30 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        tree_dict = defaultdict(list)
-        def dfs(root, level):
-            if not root:
-                return
-            dfs(root.left,level+1)
-            tree_dict[level].append(root.val)
-            dfs(root.right,level+1)
-        dfs(root, 0)
+        # tree_dict = defaultdict(list)
+        # def dfs(root, level):
+        #     if not root:
+        #         return
+        #     dfs(root.left,level+1)
+        #     tree_dict[level].append(root.val)
+        #     dfs(root.right,level+1)
+        # dfs(root, 0)
+        # res = []
+        # for i in range(0,len(tree_dict)):
+        #     res.append(tree_dict[i][-1])
+        # return res
+
+        # BFS
+        q = deque([root])
         res = []
-        for i in range(0,len(tree_dict)):
-            res.append(tree_dict[i][-1])
-        return res
+        while q:
+            qlen = len(q)
+            last_node_level = None
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    last_node_level = node.val
+                    q.append(node.left)
+                    q.append(node.right)
+            res.append(last_node_level)
+        return res[:-1]
