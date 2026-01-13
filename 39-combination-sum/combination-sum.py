b0d7b24d0abd207a23733af1df493427fca78504
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 15:44:29 -0800

    Time: 15 ms (28.65%) | Memory: 19.6 MB (9.51%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        n = []
-        subSet = []
-        def helper(i,cur_sum):
-            if cur_sum == target:
-                n.append(subSet.copy())
+        res = set()
+        cur_arr = []
+        def helper(i, total):
+            if total > target or i >= len(candidates):
                 return
-            if cur_sum > target or i >= len(candidates):
-                return
-            subSet.append(candidates[i])
-            helper(i, cur_sum+candidates[i])
-            subSet.pop()
-            helper(i+1, cur_sum)
+            if total == target:
+                res.add(tuple(cur_arr.copy()))
+            #Deciding to take the current i we are on
+            cur_arr.append(candidates[i])
+            helper(i, total+candidates[i])
+            #Deciding not to take the current i we are on
+            cur_arr.pop()
+            helper(i+1, total)
         helper(0,0)
-        return n
+        return list(res)
