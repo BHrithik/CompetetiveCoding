8afa4cb050f9c5a9ebe5d0d5e042e72ea2573669
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 21:17:41 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (10.66%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,33 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        length = n*2
         res = []
-        def helper(s, left, right):
-            if len(s) == length:
-                res.append(s)
+        stack = []
+        def helper(open_c, close_c):
+            if open_c == close_c == n:
+                res.append("".join(stack))
                 return
-            if left < n:
-                helper(s+"(", left+1,right)
-            if right < left:
-                helper(s+")", left, right+1)
-        helper("",0,0)
+            if open_c < n:
+                stack.append("(")
+                helper(open_c+1, close_c)
+                stack.pop()
+            if close_c < open_c:
+                stack.append(")")
+                helper(open_c, close_c+1)
+                stack.pop()
+        helper(0,0)
         return res
-        # length = n*2
-        # res = set()
-        # cache = defaultdict(list)
-        # def helper(s,left,right):
-        #     if len(s) == length and s not in res:
-        #         res.add(s)
-        #         return
-        #     if left == right:
-        #         cache[(left,right)] = s
-        #         if (n-left,n-right) in cache:
-        #             if s+cache[(n-left,n-right)] not in res:
-        #                 res.add(s+cache[(n-left,n-right)])
-        #                 print(f"{cache[(n-left,n-right)]} found in cache for {s}")
-        #     if left < n:
-        #         helper(s+"(",left+1, right)
-        #     if right < left:
-        #         helper(s+")",left,right+1)
-        # helper("",0,0)
-        # return list(res)
