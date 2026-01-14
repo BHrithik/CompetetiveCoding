08e1c95f1c92a7b4dd34d5b18dd19ea685a33662
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 18:30:22 -0800

    Time: 35 ms (13.00%) | Memory: 19.6 MB (6.05%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,50 +1,19 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        res = []
-        combination = []
-        def dfs(i, a):
-            if a == target:
-                res.append((combination.copy()))
+        res =  set()
+        def helper(i, cur, total):
+            if total == target:
+                res.add(tuple(cur.copy()))
                 return
-            if i > len(candidates)-1 or a > target: return
-            combination.append(candidates[i])
-            dfs(i+1,a+candidates[i])
-            combination.pop()
+            if total > target or i == len(candidates):
+                return
+            # Lets include the current number we are on
+            cur.append(candidates[i])
+            helper(i+1, cur, total+candidates[i])
+            cur.pop()
             while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
-            dfs(i+1,a)
-        dfs(0,0)
+            helper(i+1, cur, total)
+        helper(0, [], 0)
         return list(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # candidates.sort()
-        # res = []
-        # subSet = []
-        # def bcktrk(i,cur_sum):
-        #     if cur_sum == target:
-        #         res.append(subSet.copy())
-        #         return
-        #     if cur_sum > target or i>= len(candidates):
-        #         return
-        #     subSet.append(candidates[i])
-        #     bcktrk(i+1,cur_sum+candidates[i])
-        #     subSet.pop()
-        #     while i+1 < len(candidates) and candidates[i+1] == candidates[i]:
-        #         i = i+1
-        #     bcktrk(i+1, cur_sum)
-        # bcktrk(0,0)
-        # return list(res)
