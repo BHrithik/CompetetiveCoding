5ed169b4eaaad1e09bb6f9764f1f14bc2c2c36a5
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 21:24:39 -0800

    Time: 249 ms (48.70%) | Memory: 21.7 MB (37.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,17 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res = 0
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
         def helper(i,j):
-            if not 0 <= i < len(grid) or not 0 <= j < len(grid[i]) or grid[i][j] == "0":
+            if not (0 <= i < len(grid)) or not (0 <= j < len(grid[0])) or grid[i][j] == "0":
                 return
-            grid[i][j] = "0"
-            for di, dj in directions:
-                helper(i+di,j+dj)
-
+            grid[i][j] = "0" # marking as visited so that we don't come here again
+            helper(i+1,j) # exploring right
+            helper(i-1,j) # exploring left
+            helper(i,j+1) # exploring up
+            helper(i,j-1) # exploring down
+            return
+        count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    helper(i,j)
-                    res += 1
-        return res
+                    count += 1 # We found land
+                    helper(i,j) # let us explore how big this island is mark them as visited
+        return count
