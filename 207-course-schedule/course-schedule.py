30b8246210cfea66371cde35b514bc2c16823dd7
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-14 00:19:00 -0800

    Time: 7 ms (44.91%) | Memory: 21.5 MB (5.94%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,59 +1,25 @@
 class Solution:
-    def canFinish(self, numCourses: int, preQ: List[List[int]]) -> bool:
-        courseDict = {}
-        for c1,c2 in preQ:
-            if c1 not in courseDict:
-                courseDict[c1] = []
-            courseDict[c1].append(c2)
-        cache = {}
-        def isCycle(course, visited):
-            if course not in courseDict:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preqs = {i:[] for i in range(numCourses)}
+        for cor, pre in prerequisites:
+            preqs[cor].append(pre)
+        
+        visited_courses = set()
+        def dfs(crs):
+            if crs in visited_courses:
                 return False
-            if course in visited:
+            if preqs[crs] == []:
                 return True
-            if course in cache:
-                return cache[course]
-            visited.add(course)
-            for pre in courseDict[course]:
-                if isCycle(pre, visited):
-                    cache[course] = True
-                    return True
-            visited.remove(course)
-            cache[course] = False
-            return False
-
-        for course in range(numCourses):
-            if isCycle(course,set()):
+            
+            visited_courses.add(crs)
+            for pre in preqs[crs]:
+                if not dfs(pre):
+                    return False
+            visited_courses.remove(crs)
+            preqs[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
         return True
-
-
-
-
-
-
-
-
-
-        # adjArr = defaultdict(list)
-        # for i in :
-        #     adjArr[i[0]].append(i[1])
-        # print(adjArr)
-        # visitSet = set()
-        # print("DFS")
-        # def dfs(crs):
-        #     if crs in visitSet:
-        #         return False
-        #     if adjArr[crs] == []:
-        #         return True
-        #     visitSet.add(crs)
-        #     for i in adjArr[crs]:
-        #         if not dfs(i):
-        #             return False
-        #     visitSet.remove(crs)
-        #     adjArr[crs] = []
-        #     return True
-        # for i in range(numCourses):
-        #     if not dfs(i):
-        #         return False
-        # return True
