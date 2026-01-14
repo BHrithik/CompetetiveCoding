1e995850bfc800e11beb5c0d17bb69f600391c14
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-13 18:49:30 -0800

    Time: 9 ms (4.74%) | Memory: 19.3 MB (8.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        i = 0
-        stackDict = {")":"(","}":"{","]":"["}
+        concurent_dict = {"{":"}","(":")","[":"]"}
         for i in s:
-            if i in stackDict:
-                if stack and stack[-1] == stackDict[i]:
-                    stack.pop()
+            if i in concurent_dict.keys():
+                print(f"Adding {i} to stack")
+                stack.append(i)
+            elif stack:
+                most_recent = stack.pop()
+                print(f"Checking if most recent in stack i.e. {most_recent} if comlimented with {i}")
+                if concurent_dict[most_recent] == i:
+                    continue
                 else:
                     return False
             else:
-                stack.append(i)
-        return stack == []
+                return False
+        return True if not stack else False
