dfcb086faf017bd7bd26d2993c749b96492e0b2d
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-25 20:17:49 -0800

    Time: 0 ms (100.00%) | Memory: 19.7 MB (11.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        if len(nums) <=1:
             return nums[0]
-        def robb(nums):
-            cache = {}
-            def dfs(i):
+        def rob2(nums:List[int]) -> int:
+            memo = {}
+            def helper(i):
                 if i >= len(nums):
                     return 0
-                if i in cache:
-                    return cache[i]
-                cache[i] = nums[i] + max(dfs(i+2),dfs(i+3))
-                return cache[i]
-            return max(dfs(0),dfs(1))
-        return max(robb(nums[:-1]),robb(nums[1:]))
+                if i in memo:
+                    return memo[i]
+                rob_current = nums[i]+helper(i+2)
+                skip_current = helper(i+1)
+                memo[i] = max(rob_current, skip_current)
+                return memo[i]
+            return helper(0)
+        return max(rob2(nums[1:]),rob2(nums[:-1]))
