975e997103cbb7ebd8aa53f15352e559f355719c
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-26 00:18:30 -0800

    Time: 918 ms (27.18%) | Memory: 73.6 MB (5.46%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,35 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        cache = {}
-        def helper(cur_amount):
-            if cur_amount == 0:
+        coins.sort(reverse=True)
+        fin_res = float('inf')
+        memo = {}
+        def helper(rem_amt):
+            if rem_amt > amount:
+                return float('inf')
+            if rem_amt == amount:
                 return 0
-            if cur_amount < 0:
-                return amount+1
-            if cur_amount in cache:
-                return cache[cur_amount]
-            res = amount+1
+            if rem_amt in memo:
+                return memo[rem_amt]
+            res = float('inf')
             for coin in coins:
-                res = min(res,helper(cur_amount-coin)+1)
-            cache[cur_amount] = res
-            return cache[cur_amount]
-        res = helper(amount)
-        return res if res != amount+1 else -1
-
-
-
-
-
-
-
-
-
-
-
-        dp = [amount+1]*(amount+1)
-        dp[0] = 0
-        for a in range(1,amount+1):
-            for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(dp[a],1+dp[a-coin])
-        return dp[amount] if dp[amount] != amount+1 else -1
+                res = min(res, 1+helper(rem_amt+coin))
+            memo[rem_amt] = res
+            return res
+        fin_res = helper(0)
+        return fin_res if fin_res != float('inf') else -1
