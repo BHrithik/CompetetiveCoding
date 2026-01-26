e9335cf6c9816ecd3afd9d026ea3901ebc3f62a2
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-25 20:20:18 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (11.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <=1:
-            return nums[0]
-        def rob2(nums:List[int]) -> int:
-            memo = {}
-            def helper(i):
-                if i >= len(nums):
-                    return 0
-                if i in memo:
-                    return memo[i]
-                rob_current = nums[i]+helper(i+2)
-                skip_current = helper(i+1)
-                memo[i] = max(rob_current, skip_current)
+        if len(nums) <= 2:
+            return max(nums)
+        memo = {}
+        def helper(i, arr):
+            if i >= len(arr):
+                return 0
+            if i in memo:
                 return memo[i]
-            return helper(0)
-        return max(rob2(nums[1:]),rob2(nums[:-1]))
+            rob_current = arr[i]+helper(i+2, arr)
+            skip_current = helper(i+1, arr)
+            memo[i] = max(rob_current, skip_current)
+            return memo[i]
+        skip_first_house = helper(0,nums[1:])
+        memo = {}
+        skip_last_house = helper(0, nums[:-1])
+        return max(skip_first_house, skip_last_house)
