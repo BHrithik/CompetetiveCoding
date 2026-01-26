39e8b615398d853a7adb64f220f476c634aa221a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-25 23:18:28 -0800

    Time: 3 ms (28.87%) | Memory: 19.6 MB (8.00%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,23 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0' or "00" in s:
+        if not s or s[0] == '0':
             return 0
         n = len(s)
-        memo = {}
+        memo = {n: 1}
         def helper(i):
-            if i == n:
-                return 1
-            if s[i] == '0':
-                return 0
             if i in memo:
                 return memo[i]
-
-            ways = helper(i + 1)
-
-            if i + 1 < n:
-                two = int(s[i:i+2])
-                if 10 <= two <= 26:
-                    ways += helper(i + 2)
-            memo[i] = ways
-            return ways
+            if s[i] == "0":
+                return 0
+            res = helper(i + 1)
+            if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                res += helper(i + 2)
+            memo[i] = res
+            return res
         return helper(0)
