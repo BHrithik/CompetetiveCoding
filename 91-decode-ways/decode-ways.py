6538669b9d0177cbfe1f6437844eab46063fa07a
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-25 23:05:06 -0800

    Time: 0 ms (100.00%) | Memory: 19.7 MB (8.00%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,47 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # #12106
-        # res = 0
-        cache = {}
-        def dfs(i):
-            if i == len(s):
+        if not s or s[0] == '0' or "00" in s:
+            return 0
+        n = len(s)
+        memo = {}
+        def helper(i):
+            if i == n:
                 return 1
-            if s[i] == "0":
+            if s[i] == '0':
                 return 0
-            if i in cache:
-                return cache[i]
-            cache[i] = dfs(i+1)
-            if i+1 < len(s) and int(s[i] + s[i+1]) <= 26:
-                cache[i] += dfs(i+2)
-            return cache[i]
-        return dfs(0)
-        # val1, val2 = 0, 0
-        # for i in range(len(s)):
-            
-        dp = {}
-        def dfs(i):
-            if i == len(s):
-                return 1
-            if i in dp:
-                return dp[i]
-            if s[i] == "0":
-                return 0
-            res = dfs(i+1)
-            if i+1 < len(s) and int(s[i]+s[i+1]) <= 26:
-                res += dfs(i+2)
-            dp[i] = res
-            return dp[i]
-        return dfs(0)
+            if i in memo:
+                return memo[i]
 
+            ways = helper(i + 1)
 
-            
-
-
-
-
-
-
-
-
-
-
+            if i + 1 < n:
+                two = int(s[i:i+2])
+                if 10 <= two <= 26:
+                    ways += helper(i + 2)
+            memo[i] = ways
+            return ways
+        return helper(0)
