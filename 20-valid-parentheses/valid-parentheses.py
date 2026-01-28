ad9b326f8052cb9b17b223cbf2e6f071eb3b0d8d
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-27 22:45:16 -0800

    Time: 3 ms (36.74%) | Memory: 19.5 MB (11.45%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,16 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        concurent_dict = {"{":"}","(":")","[":"]"}
-        for i in s:
-            if i in concurent_dict.keys():
-                stack.append(i)
-            elif stack:
-                most_recent = stack.pop()
-                if concurent_dict[most_recent] == i:
-                    continue
+        get_opposite = {"{":"}","(":")","[":"]"}
+        i = 0
+        while i < len(s):
+            if s[i] in get_opposite:
+                stack.append(s[i])
+            else:
+                if stack and s[i] in get_opposite.values() and s[i] == get_opposite[stack[-1]]:
+                    stack.pop()
                 else:
                     return False
-            else:
-                return False
-        return True if not stack else False
+            i += 1
+        if stack:
+            return False
+        return True
