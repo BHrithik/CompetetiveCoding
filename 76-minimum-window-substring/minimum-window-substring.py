e032e9e6fb21fd4a52a57383cc87d5f9fef6dfc9
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-28 22:47:49 -0800

    Time: 63 ms (78.00%) | Memory: 19.6 MB (38.90%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,28 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s)<len(t):
+        if len(t) > len(s) or t == "" or s == "":
             return ""
-        tCount = Counter(t)
-        sCount = {}
-        l = 0
-        minLen = float('inf')
-        minRes = ""
+        t_dict = {}
+        for char in t:
+            t_dict[char] = t_dict.get(char,0)+1
         have = 0
-        need = len(tCount)
-        for r in range(0,len(s)):
-            if s[r] in tCount:
-                sCount[s[r]] = sCount.get(s[r],0)+1
-                if sCount[s[r]] == tCount[s[r]]:
-                    have += 1
-                while have == need:
-                    if r-l+1 < minLen:
-                        minLen = r-l+1
-                        minRes = s[l:r+1]
-                    if s[l] in sCount:
-                        sCount[s[l]] -= 1
-                    if s[l] in sCount and sCount[s[l]] < tCount[s[l]]:
-                        have -= 1
-                    l += 1
-        return minRes
-            
-        
+        need = len(t_dict)
+        l = 0
+        s_dict = {}
+        res, resLen = [-1,-1], float('inf')
+        for r in range(len(s)):
+            s_dict[s[r]] = s_dict.get(s[r],0)+1
+            if s[r] in t_dict and s_dict[s[r]] == t_dict[s[r]]:
+                have += 1
+            while have == need:
+                if (r-l+1) < resLen:
+                    resLen = r-l+1
+                    res = [l,r]
+                s_dict[s[l]] -= 1
+                if s[l] in t_dict and s_dict[s[l]] < t_dict[s[l]]:
+                    have -= 1
+                l+=1
+        l,r = res
+        return s[l:r+1] if resLen != float('inf') else ""
