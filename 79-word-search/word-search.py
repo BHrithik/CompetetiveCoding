2d8ff457147c25c27a7da56f28138e543def711b
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-01-30 21:30:16 -0800

    Time: 5256 ms (16.72%) | Memory: 19.7 MB (13.31%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,23 +1,22 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        l = len(board)
-        b = len(board[0])
-        matrix = defaultdict(list)
+        visited = set()
+        def search(i,j,index,word):
+            if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or (i,j) in visited or board[i][j] != word[index]:
+                return False
+            if index == len(word)-1:
+                return True # found complete string
+            visited.add((i,j))
+            directions = [(1,0),(0,1),(-1,0),(0,-1)]
+            for di,dj in directions:
+                if search(i+di,j+dj,index+1,word):
+                    return True
+            visited.remove((i,j))
+
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                matrix[board[i][j]].append((i,j))
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        def helper(i,j,rem_word,paths):
-            if rem_word == "":
-                return True
-            for di,dj in directions:
-                ni,nj = i+di, j+dj
-                if 0 <= ni < l and 0 <= nj < b:
-                    if board[ni][nj] == rem_word[0] and (ni,nj) not in paths and helper(ni,nj,rem_word[1:],paths|{(ni,nj)}):
+                if board[i][j] == word[0]:
+                    if search(i,j,0,word):
                         return True
-            return False
-
-        for i,j in matrix[word[0]]:
-            if helper(i,j,word[1:],{(i,j)}):
-                return True
         return False
