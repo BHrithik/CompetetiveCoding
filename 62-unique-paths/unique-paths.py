05196abba5d5b85b3ddfa0b2d10a3137762abac6
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-02-03 01:25:39 -0800

    Time: 1 ms (31.60%) | Memory: 19.4 MB (20.74%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,12 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        matrix = [[float('inf')]*n for _ in range(0,m)]
-        for i in range(m):
-            matrix[i][n-1] = 1
-        for i in range(n):
-            matrix[m-1][i] = 1
-        for i in range(m-2,-1,-1):
-            for j in range(n-2,-1,-1):
-                matrix[i][j] = matrix[i+1][j]+matrix[i][j+1]
-        print(matrix)
-        return matrix[0][0]
+        board = [[0] * n for _ in range(m)]
+        for i in range(0,len(board)):
+            board[i][n-1] = 1
+        for i in range(0,len(board[0])):
+            board[m-1][i] = 1
+        for i in range(len(board)-2,-1,-1):
+            for j in range(len(board[0])-2,-1,-1):
+                if i+1 > len(board)-1:
+                    sum1 = 0
+                else:
+                    sum1 = board[i+1][j]
+                if j+1 > len(board[0])-1:
+                    sum2 = 0
+                else:
+                    sum2 = board[i][j+1]
+                board[i][j] = sum1 + sum2
+
+        return board[0][0]
