a6fd3a9fb11ce729c2c23f2ef79fc386cb7f3dc0
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-02-04 18:35:34 -0800

    Time: 44 ms (5.61%) | Memory: 22.1 MB (50.15%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         new_dict = {}
+        # actually a thing i have been thinking about for a long time
+        def convert_str_int(word):
+            sum = ""
+            items = sorted(Counter(word).items())
+            for key,value in items:
+                sum += chr(value)+key
+            return sum
+
         for word in strs:
-            word_dict = Counter(word)
-            key = tuple(sorted(word_dict.items()))
-            if key not in new_dict:
-                new_dict[key] = []
-            new_dict[key].append(word)
+            str_key = convert_str_int(word)
+            if str_key not in new_dict:
+                new_dict[str_key] = []
+            new_dict[str_key].append(word)
         return list(new_dict.values())
