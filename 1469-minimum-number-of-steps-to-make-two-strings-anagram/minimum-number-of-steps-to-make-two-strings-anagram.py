44770eeee3969f09b8a4503fce51b71350d2fc2f
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-02-09 00:14:24 -0800

    Time: 129 ms (33.22%) | Memory: 19.6 MB (60.60%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,7 +1,10 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        s_dict = Counter(s)
-        t_dict = Counter(t)
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+        for i in range(0,len(s)):
+            s_dict[s[i]] += 1
+            t_dict[t[i]] += 1
         # we know the length of s and t are the same
         # basically need to check what we have and what we don't we need to get everything we don't have by changing
         not_have = 0
