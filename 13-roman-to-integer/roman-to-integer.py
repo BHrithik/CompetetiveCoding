76fac71b81a07bcf844a47e6c4dd606ab1e44330
Hrithik <31255192+BHrithik@users.noreply.github.com>
2026-02-09 14:02:20 -0800

    Time: 7 ms (42.61%) | Memory: 19.3 MB (54.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,36 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        ans = []
-        if 'IV' in s:
-            s=s.replace('IV','4,')
-        if 'IX' in s:
-            s=s.replace('IX','9,')
-        if 'XL' in s:
-            s=s.replace('XL','40,')
-        if 'XC' in s:
-            s=s.replace('XC','90,')
-        if 'CD' in s:
-            s=s.replace('CD','400,')
-        if 'CM' in s:
-            s=s.replace('CM','900,')
-        if 'I' in s:
-            s=s.replace('I','1,')
-        if 'V' in s:
-            s=s.replace('V','5,')
-        if 'X' in s:
-            s=s.replace('X','10,')
-        if 'L' in s:
-            s=s.replace('L','50,')
-        if 'C' in s:
-            s=s.replace('C','100,')
-        if 'D' in s:
-            s=s.replace('D','500,')
-        if 'M' in s:
-            s=s.replace('M','1000,')
-        print(s)
-        ans = 0
-        nums=s.split(',')
-        for i in nums:
-            if i:
-                ans = ans + int(i)
-        return ans
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        for i in range(len(s)):
+            # If next value is greater, subtract current
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+
+        return total
